0acf79a9a1a062d615aef9eb936314bfd6835851
Dilawar Singh_10407007_ <dilawar@sharada.(none)>
2012-05-29 17:15:28 +0530

    Updated the interface of form.

--- eeta.php
+++ eeta.php
@@ -1,19 +1,31 @@
 <html>
 <body>
 <h1>EE IITB Teaching Assistant Interface</h1>
-<h2>Authentication</h2>
-Please fill in your LDAP details. <br>
-Note that we do not save your password. This is to authenticate you with proxy-server. 
+<font size="3" color="black">
+Please fill in your LDAP details. </font><br>
+<font size="2" color="red">Note that we do not save your password. This is to authenticate you with proxy-server. </font>
 <br>
 <br>
 <form action="authenticate.php" method="post">
-LDAP ID : <input type="text" name="username" id="username" />
-Password : <input type="password" size="25" name="pass" id="pass">
-Semester : <select name="sem">
-	<option value="2012-13-1">Sem 1, 2012-13</option>
-	<option value="2011-12-2">Sem 2, 2011-12</option>
-	<option value="2011-12-1">Sem 1, 2011-12</option>
+Proxy Username : <input type="text" name="username" id="username" />
+Proxy Password : <input type="password" size="25" name="pass" id="pass"> <br><br>
+Semester : <select name="semester"> 
+	<option value="2012">2012</option>
+	<option value="2011">2011</option>
+	<option value="2010">2010</option>
+</select> 
+<select>
+	<option value="Odd">Odd</option>
+	<option value="Even">Even</option>
+	<option value="Summer">Summer</option>
 </select>
+<!-- <input type="radio" name="Semster" value="Odd" checked="checked" >Odd
+<input type="radio" name="Semester" value="Even" >Even
+<input type="radio" name="Semester" value="Summer" >Summer<br /> -->
+<br>
+<br>
+<input type="radio" name="Role" value="Student" checked="checked" />Student
+<input type="radio" name="Role" value="Teacher" />Teacher<br />
 <input type="submit" name="Submit" value="Submit" />
 </form>
 </body>
