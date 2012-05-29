8860183099e88023480ac53798ac86fce44e5ca0
Dilawar Singh_10407007_ <dilawar@sharada.(none)>
2012-05-29 16:07:10 +0530

    This is almost ready.

--- authenticate.php
+++ authenticate.php
@@ -6,7 +6,16 @@
 
 $proxy_user=$_REQUEST["username"];
 $proxy_pass=$_REQUEST["pass"];
+$acad_sem=$_REQUEST["sem"];
+$db_name="ta".$acad_sem;
+$course_list="./courses/course_".$acad_sem.".txt";
 $base_url="http://www.ee.iitb.ac.in/student/~dilawar/Scripts/";
+
+#for dedault case.
+if(strlen($db_name) < 2)
+{
+	$db_name="ta"."2011-12-2";
+}
 
 if(strlen($proxy_user) < 2) {
 	$proxy_user=getenv('proxy_username');
@@ -57,27 +66,93 @@ if($res) {
 	$location=$_SERVER['PATH_INFO'];
 	$sqlpass=$_SERVER['sql_pass'];
 	if(strlen($sqlpass) < 2) {
-		$sqlpass="rashmirathi";
+		$sqlpass="dilawar123";
 	}
-	$con = mysql_connect("10.107.105.13", "root", $sqlpass);
+	$con = mysql_connect("10.107.105.13", "dilawar", $sqlpass);
 	if(!$con) {
 		echo "\nIt is embarrasing but I can not connect to database!\n Redirecting in 3 seconds...\n";
 		header("Refresh: 3, url=$base_url./eeta.php");
 	}
 	else {
+
 		# check if entry for the username already exists.
-		mysql_query("CREATE DATABASE IF NOT EXISTS ta2012", $con);
+		mysql_query("CREATE DATABASE IF NOT EXISTS $db_name", $con);
 		mysql_select_db("ta2012", $con);
-		$sql= "CREATE TABLE IF NOT EXISTS tas 
-			( LDAP varchar(20),
-				FirstName varchar(20),
-				LastName varchar(20),
-				PRIMARY KEY(LDAP))";
+		$sql= "CREATE TABLE IF NOT EXISTS job_table
+			( LDAP varchar(20)
+				, FirstName varchar(20)
+				, LastName varchar(20)
+				, WhichSem int
+				, FirstChoice varchar(6)
+				, SecondChoice varchar(6)
+				, ThirdChoice varchar(6)
+				, prevJob varchar(6)
+				, pprevJob varchar(6)
+				, ppprevJob varchar(6)
+				, PRIMARY KEY(LDAP)
+			)";
 		mysql_query($sql, $con);
-		mysql_close($con);
+		mysql_query("use job_table");
 
 	}
 }
-
 ?>
 
+<?php 
+echo $course_list;
+$fh = fopen($course_list, "r");
+if(!$fh) {
+	echo "<br>No course list is available with us for this academic semester. Redirecting in 3 second...<br>";
+	header("Refresh: 3, url=$base_url./eeta.php");
+}
+$course_array = array();
+while(! feof($fh))
+{
+	$data=fgetcsv($fh, 100, ",");
+	array_push($course_array, $data);
+}
+if(!course_array) {
+	echo "<br>Error in reading course list. Redirecting in 3 second...<br>";
+	header("Refresh: 3, url=$base_url./eeta.php");
+}
+$courses = $course_array;
+?>
+
+<html>
+<head>
+<style type="text/css">
+	.container{
+		width : 200px;
+		clear : both;
+	}
+	.container input {
+		width : 30%;
+		clear : both;
+}
+</style>
+</head>
+<body>
+<div class="container">
+<h3>Job description</h3>
+<form action="database.php" method="post">
+First Preference :
+<?php echo generateSelect("ta1", $courses);	?>
+Second Preference :
+<?php echo generateSelect("ta2", $courses);	?>
+Third Preference :
+<?php echo generateSelect("ta3", $courses);	?>
+<input type="submit" name="Submit" value="Submit" />
+</form>
+</body>
+</html>
+
+<?php 
+## This function converts course list into select options.
+function generateSelect($name, $courses) {
+	$html = "<select name=".$name.">";
+	foreach($courses as $id => $cname) {
+		$html .= "<option value=".$cname[0].">".$cname[0].": ".$cname[1]."</option>";
+	}
+	$html .= "</select>";
+	return $html;
+}
--- eeta.php
+++ eeta.php
@@ -9,6 +9,11 @@ Note that we do not save your password. This is to authenticate you with proxy-s
 <form action="authenticate.php" method="post">
 LDAP ID : <input type="text" name="username" id="username" />
 Password : <input type="password" size="25" name="pass" id="pass">
+Semester : <select name="sem">
+	<option value="2012-13-1">Sem 1, 2012-13</option>
+	<option value="2011-12-2">Sem 2, 2011-12</option>
+	<option value="2011-12-1">Sem 1, 2011-12</option>
+</select>
 <input type="submit" name="Submit" value="Submit" />
 </form>
 </body>
