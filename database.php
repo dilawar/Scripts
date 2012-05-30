10c0929b41fb3064ece68b7303a9c4786133df63
Dilawar <dilawar.iitb@gmail.com>
2012-05-30 22:03:31 +0530

    We can now update the database.

--- database.php
+++ database.php
@@ -2,16 +2,14 @@
 
 <?php
 include('error.php');
-session_save_path(getenv('HOME'."/sessions"));
+include('func.php');
 session_start();
 
-$ldap = $_SESSION['user_ldap'];
+$ldap = $_SESSION['ldap'];
 $init = $_SESSION['init'];
 $ip = $init['db_ip'];
 $pass = $init['db_pass'];
 $user = $init['db_user'];
-
-print_r($_POST);
 
 $con = mysql_connect($ip, $user, $pass);
 if(!$con) {
@@ -26,32 +24,57 @@ else
 	$_program = $_POST['program'];
 	$_category = $_POST['category'];
 	$_gradYear = $_POST['gradYear'];
-	$_gradSem = $_POST['gradSem'];
-	$_graduatingOn = "";
-	if($_gradSem == "even") {
-		$_graduatingOn = "1 September ".$_gradYear;
-	}
-	else {
-		$_graduatingOn = "1 January ".$_gradYear;
-	}
-	$_graduatingOn = date("Y-m-d", strtotime($_graduatingOn));	
+	$_graduatingOn = $_POST['graduatingOn'];	
 	
-	$query = sprintf("insert into student (ldap, roll, program, category, graduatingOn) 
-			values ('%s', '%s', '%s', '%s', '%s' )", 
-			mysql_real_escape_string($_ldap)
-		, mysql_real_escape_string($_roll)
-		, mysql_real_escape_string($_program)
-		, mysql_real_escape_string($_category)
-		, mysql_real_escape_string($_graduatingOn)
-		);
+	/* check if entry is already there. */
+	$query = sprintf("select * from student where ldap='%s'",
+				mysql_real_escape_string($_ldap));
 	$res = mysql_db_query("eestudents", $query, $con);
-
 	if(!$res) {
-		echo printErrorSevere("I can not add you now ! Redirecting in 3 sec...");
-		header("Refresh: 3, url=$base_url./eeta.php");
-}
+		echo "Error in query :".mysql_error();
+	}
 	else {
-		header("Location: ".$_SESSION['base_url']."/preference.php");
+		$entry = mysql_fetch_assoc($res);
+		if(count($entry) > 0) 
+		{
+			echo "Updating your entry ...<br>";
+			$query = sprintf("update student set ldap='%s', roll = '%s', program='%s', category='%s', graduatingOn='%s'"
+				, mysql_real_escape_string($_ldap)
+				, mysql_real_escape_string($_roll)
+				, mysql_real_escape_string($_program)
+				, mysql_real_escape_string($_category)
+				, mysql_real_escape_string($_graduatingOn)
+			);
+			$res = mysql_db_query("eestudents", $query, $con);
+			if(!$res) {
+				echo "Failed to update your entry with error".mysql_error();
+			}
+			else {
+				echo "<b> Successfully updated your entry <br>";
+			}
+		}
+		else {
+			echo "Inserting your entry into database ... <br>";
+			$query = sprintf("insert into student (ldap, roll, program, category, graduatingOn) 
+				values ('%s', '%s', '%s', '%s', '%s' )", 
+				mysql_real_escape_string($_ldap)
+				, mysql_real_escape_string($_roll)
+				, mysql_real_escape_string($_program)
+				, mysql_real_escape_string($_category)
+				, mysql_real_escape_string($_graduatingOn)
+			);
+			$res = mysql_db_query("eestudents", $query, $con);
+
+			if(!$res) {
+				echo printErrorSevere("I can not add you now ! Redirecting in 3 sec...");
+				echo mysql_error();
+				header("Refresh: 3, url=$base_url./eeta.php");
+
+			}
+			else {
+				echo "I have successfully added you to my database.";
+			}
+		}
 	}
 }
 
