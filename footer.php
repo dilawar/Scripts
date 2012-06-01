f872f672355192edb49483e1cc6535a2ad58bac6
dilawar <dilawar.iitb@gmail.com>
2012-06-02 01:04:05 +0530

    Moved project to another place.

--- footer.php
+++
@@ -1,3 +0,0 @@
-<html>
-<small>Developed and maintained by Dilawar Singh. Email <pre>dilawar@ee.iitb.ac.in </pre></small>
-</html>
--- header.php
+++
@@ -1,8 +0,0 @@
-<html> 
-<body>
-<h2> EE Dept : Teaching Assistant Interface </h2>
-<h6> (c) Dilawar Singh, 2012 </h6>
-<hr />
-<br>
-</body>
-</html>
