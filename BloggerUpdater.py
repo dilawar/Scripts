57d6cc0d89ca064a0db5b9c913915680f3036018
Dilawar Singh <dilawar@ee.iitb.ac.in>
2012-10-10 03:53:29 +0530

    Added google blog updater.

---
+++ BloggerUpdater.py
@@ -0,0 +1,49 @@
+'''
+Created on Apr 17, 2012
+@author: Vlad Gorloff
+'''
+
+from gdata import service
+import atom
+
+"""Simple class for updating posts on www.blogger.com service"""
+class BloggerUpdater:
+    
+    """Initializing instance and login into www.blogger.com"""
+    def __init__(self, user, password):
+        self.blogger_service = service.GDataService(user, password)
+        self.blogger_service.source = 'gv-cl-blogger-updater-1.0'   # Client software name
+        self.blogger_service.service = 'blogger'
+        self.blogger_service.account_type = 'GOOGLE'
+        self.blogger_service.server = 'www.blogger.com'
+        self.blogger_service.ssl = True                             # Let's protect connection
+        print "[I] Logging into " + self.blogger_service.server + " ..."
+        self.blogger_service.ProgrammaticLogin()
+        if self.blogger_service.current_token is None:
+            print "[E] Unable to login into \"" + self.blogger_service.server + "\""
+    
+    """This will get blog entry by it's title (name)"""  
+    def GetBlogByTitle(self, title):
+        query = service.Query()
+        query.feed = '/feeds/default/blogs'
+        feed = self.blogger_service.Get(query.ToUri())
+        for entry in feed.entry:
+            if entry.title.text == title:
+                self.blog_id = entry.GetSelfLink().href.split("/")[-1]
+                return entry
+        return None
+    
+    """This will get post entry by it's title (name)"""
+    def GetPostByTitle(self, title):
+        feed = self.blogger_service.GetFeed('/feeds/' + self.blog_id + '/posts/default')
+        for entry in feed.entry:
+            if entry.title.text == title:
+                return entry
+        return None
+    
+    """This will update supplied post entry with new content
+    Updated post entry returned as result"""
+    def UpdatePost(self, postEntry, newContent):
+        contentType = postEntry.content.type
+        postEntry.content = atom.Content(contentType, None, newContent)
+        return self.blogger_service.Put(postEntry, postEntry.GetEditLink().href)
---
+++ updater.py
@@ -0,0 +1,100 @@
+'''
+Created on Apr 17, 2012
+@author: Vlad Gorloff
+'''
+
+import os
+import sys
+import getopt
+import re
+import BloggerUpdater
+
+def usage():
+    print >>sys.stderr, "Usage:"
+    print >>sys.stderr, "-h, --help - this help"
+    print >>sys.stderr, "-b, --blog - name of existing blog"
+    print >>sys.stderr, "-p, --post - name of existing post"
+    print >>sys.stderr, "-s, --src  - path to HTML file with replacement content"
+    print >>sys.stderr, "--user     - Blogger account name"
+    print >>sys.stderr, "--pass     - Blogger account password"
+    print >>sys.stderr, ""
+      
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    
+    # Getting command line arguments   
+    try:
+        opts, args = getopt.getopt(argv[1:], "b:p:s:h",
+                                   ["blog", "post", "src", "user=", "pass=", "help"])
+    except getopt.GetoptError, err:
+        print str(err)
+        usage()
+        return 1
+
+    if len(opts) <= 0: # Inconsistent number of command line arguments
+        usage()
+        return 1
+    
+    # Retrieving arguments
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+            return 0
+        elif o in ("-b", "--blog"):
+            blog = a
+        elif o in ("-p", "--post"):
+            post = a
+        elif o in ("-s", "--src"):
+            src = a
+        elif o in ("--user"):
+            user = a
+        elif o in ("--pass"):
+            password = a
+        else:
+            print "unknown option: " + o
+            usage()
+            return 1
+        
+    # Checking required arguments (if variable not exist NameError raised)
+    try:
+        blog
+        post
+        src
+        user
+        password
+    except NameError:
+        usage()
+        return 1
+    
+    content = open(src, 'r').read()              # Opening source HTML for reading
+    content = re.sub(r'>\s+<', "><", content)    # Removing whitespace between HTML tags
+    content = re.sub(r'\s+$|^\s+', "", content)  # Removing leading and trailing whitespace
+    
+    # Creating instance of BloggerUpdater
+    updater = BloggerUpdater.BloggerUpdater(user, password)
+    
+    # Getting blog entry
+    blogEntry = updater.GetBlogByTitle(blog)
+    if blogEntry is None:
+        print "[E] Unable to find requested blog: \"" + blog + "\""
+        return 1
+    print "[I] Requested blog found: \"" + blogEntry.title.text + "\""
+    
+    # Getting post entry
+    postEntry = updater.GetPostByTitle(post)
+    if postEntry is None:
+        print "[E] Unable to find requested post: \"" + post + "\""
+        return 1
+    print "[I] Requested post found: \"%s\". Last update: %s. Updating ..." \
+        % (postEntry.title.text, postEntry.updated.text)
+    
+    # Updating post with new content
+    resultEntry = updater.UpdatePost(postEntry, content)
+    print "[I] Successfully updated: \"%s\". Last update: %s. Done!" \
+        % (resultEntry.title.text, resultEntry.updated.text) 
+    
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
