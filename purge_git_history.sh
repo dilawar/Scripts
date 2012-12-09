7a90271fc80af6ec00ed52244f383fd1fc08ec5b
Dilawar Singh <dilawar@ee.iitb.ac.in>
2012-12-10 00:14:44 +0530

    Updated repo

--- purge_git_history.sh
+++ purge_git_history.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-git filter-branch --index-filter "git rm -f --cached --ignore-unmatch $1" --prune-empty --tag-name-filte cat -- --all 
+git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $1" --prune-empty --tag-name-filte cat -- --all 
