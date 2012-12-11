e96d26c632ecff8072dbde330ec81c15ad5d8077
Dilawar Singh <dilawar@ee.iitb.ac.in>
2012-12-11 21:07:54 +0530

    Script to update web on sharada automatically.

--- update_web_on_sharada.sh
+++ update_web_on_sharada.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-ssh dilawar@sharada.ee.iitb.ac.in cd public_html && git add . && git commit -m "Remove updated " && git push 
+ssh dilawar@sharada.ee.iitb.ac.in "cd public_html && git add . && git commit -m \"Remove updated \" && git push"
