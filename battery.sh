9dae4981411756ef91e187b50b5ebbe042b231f1
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-01-22 18:15:37 +0530

    Added battery.sh file to send notification about battery to desktop

--- battery.sh
+++ battery.sh
@@ -1,42 +1,30 @@
 #!/bin/bash
-#
-# by lyon8 (lyon8@gmx.net)
-# modified by dilawar@ee.iitb.ac.in
-# show your laptop battery state in dzen
- 
-BG='#444'  # dzen backgrounad
-FG='#888'  # dzen foreground
-W=150     # width of the dzen bar
-GW=80      # width of the gauge
-GFG='#999'  # color of the gauge
-GH=6       # height of the gauge
-GBG='#3f6'  # color of gauge background
-X=1060      # x position
-Y=0    # y position
-FN='fixed' # font
- 
-STATEFILE='/proc/acpi/battery/BAT0/state' # battery's state file
-INFOFILE='/proc/acpi/battery/BAT0/info'   # battery's info file
- 
-LOWBAT=25        # percentage of battery life marked as low
-LOWCOL='#ff4747' # color when battery is low
-TIME_INT=1         # time intervall in seconds
- 
-PREBAR='/usr/share/dzen2/bitmaps/battery.xbm' # caption (also icons are possible)
- 
-while true; do
-# look up battery's data
-BAT_FULL=`cat $INFOFILE|grep design|line|cut -d " " -f 11`;
-STATUS=`cat $STATEFILE|grep charging|cut -d " " -f 12`;
-RCAP=`cat $STATEFILE|grep remaining|cut -d " " -f 8`;
- 
-# calculate remaining power
-RPERCT=`expr $RCAP \* 100`;
-RPERC=`expr $RPERCT / $BAT_FULL`;
- 
-# draw the bar and pipe everything into dzen
-if [ $RPERC -le $LOWBAT ]; then GFG=$LOWCOL; fi
-$echo  $PREBAR
-eval echo $RPERC | dzen2-gdbar -h $GH -w $GW -fg $GFG -bg $GBG
-sleep $TIME_INT;
-done | dzen2 -ta c -tw $W -y $Y -x $X -fg $FG -bg $BG -fn $FN -xs 2
+
+cd ~/Scripts
+notification=$(grep 'notification:' notification|awk '{print $2}')
+
+cd /proc/acpi/ac_adapter/C240;
+power=$(grep 'state:' state|awk '{print $2}')
+s1="$power"
+
+s2="charged"
+s3="on-line"
+s4="on"
+
+export DISPLAY=:0
+
+if [ "$s1" = "on-line" ]; then
+  cd /proc/acpi/battery/C241;
+  state=$(grep 'charging state:' state|awk '{print $3}')
+  if [ $state = $s2 ] && [ "$notification" = "$s4" ];
+    then
+            notify-send  --urgency=critical "Power Manager" "battery is full" -i battery_full
+            echo "notification: off" >~/.scripts/notification
+
+    fi
+
+else
+  if [ $notification != "on" ]; then
+    echo "notification: on" >~/Scripts/notification
+  fi
+fi
