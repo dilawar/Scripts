8ad7c28daa21e8f28d3717619595e429269b8b13
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-02-27 10:44:15 +0530

    Updated python script. It now prints files larger than size specified without their mode appended.

--- sort_git_repo.py
+++ sort_git_repo.py
@@ -3,33 +3,37 @@
 # http://stackoverflow.com/questions/298314/find-files-in-git-repo-over-x-megabytes-that-dont-exist-in-head
 # 
 import os, sys
+import string
 
 def getOutput(cmd):
-    return os.popen(cmd).read()
+  return os.popen(cmd).read()
 
 if (len(sys.argv) <> 2):
-    print "usage: %s size_in_bytes" % sys.argv[0]
+  print "usage: %s size_in_bytes" % sys.argv[0]
 else:
-    maxSize = int(sys.argv[1])
+  maxSize = int(sys.argv[1])
 
-    revisions = getOutput("git rev-list HEAD").split()
+  revisions = getOutput("git rev-list HEAD").split()
 
-    bigfiles = set()
-    for revision in revisions:
-        files = getOutput("git ls-tree -zrl %s" % revision).split('\0')
-        for file in files:
-            if file == "":
-                continue
-            splitdata = file.split()
-            commit = splitdata[2]
-            if splitdata[3] == "-":
-                continue
-            size = int(splitdata[3])
-            path = splitdata[4]
-            if (size > maxSize):
-                bigfiles.add("%10d %s %s" % (size, commit, path))
+  bigfiles = set()
+  for revision in revisions:
+    files = getOutput("git ls-tree -zrl %s" % revision).split('\t')
+    for file in files:
+      if file == "":
+        continue
+      splitdata = file.split()
+      if len(splitdata) < 4 :
+        continue
+      commit = splitdata[-2]
+      if splitdata[-1] == "-":
+        continue
+      size = int(splitdata[-1])
+      path = "".join(splitdata[1:len(splitdata)-3])
+      if (size > maxSize):
+        path = path.split("\x00100")
+        bigfiles.add("%10d %s %s" % (size, commit, path[0]))
 
-    bigfiles = sorted(bigfiles, reverse=True)
+  bigfiles = sorted(bigfiles, reverse=True)
 
-    for f in bigfiles:
-        print f
+  for f in bigfiles:
+      print f
