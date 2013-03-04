de042e3d25874545ad886aa327b7ba7b01578f70
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-04 16:50:41 +0000

    Updated the lua configuration file.

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -101,7 +101,7 @@ layouts =
   awful.layout.suit.spiral.dwindle,
   awful.layout.suit.max,
   awful.layout.suit.max.fullscreen,
-  awful.layout.suit.magnifier
+  awful.layout.suit.magnifier,
   awful.layout.suit.floating,
 }
 -- }}}
