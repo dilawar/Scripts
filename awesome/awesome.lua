8971ab38169d6e1a3ded9bc2d82f3aa804498f1f
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-06 19:05:15 +0000

    Scrot to screenshot

--- awesome/awesome.lua
+++ awesome/awesome.lua
@@ -238,6 +238,7 @@ globalkeys = awful.util.table.join(
 awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
 awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
 awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
+awful.key({ }, "Print", function() awful.util.spawn("scrot -e 'mv $f ~/Pictures/shots/ 2>/dev/null'") end),
 
 awful.key({ modkey,           }, "j",
 function ()
