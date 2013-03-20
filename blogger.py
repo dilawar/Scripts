d383d342d294846f544ffc802dee5e73727b546b
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-20 21:25:18 +0530

    updated

--- blogger.py
+++ blogger.py
@@ -5,117 +5,115 @@ Created on Apr 17, 2012
 
 Modified by Dilawar for personal use.
 '''
-
+import argparse
 import os
 import sys
 import getopt
 import re
-import BloggerUpdater
-
-def usage():
-    print >>sys.stderr, "Usage:"
-    print >>sys.stderr, "-h, --help - this help"
-    print >>sys.stderr, "-b, --blog - name of existing blog"
-    print >>sys.stderr, "-p, --post - name of existing post"
-    print >>sys.stderr, "-s, --src  - path to HTML file with replacement content"
-    print >>sys.stderr, "-g, --get  - name of blog to retrieve"
-    print >>sys.stderr, "--user     - Blogger account name"
-    print >>sys.stderr, "--pass     - Blogger account password"
-    print >>sys.stderr, ""
+import blogger.BloggerUpdater as BloggerUpdater
       
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-    
-    # Getting command line arguments   
-    try:
-      opts, args = getopt.getopt(argv[1:], "p:s:g:h",
-                                   ["post", "src", "get" "help"])
-    except getopt.GetoptError, err:
-        print str(err)
-        usage()
-        return 1
+def main(args):
+  argv = sys.argv
 
-    if len(opts) <= 0: # Inconsistent number of command line arguments
-        usage()
-        return 1
-    
-    # Retrieving arguments
-    get = None;
-    src = None;
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            usage()
-            return 0
-        elif o in ("-p", "--post"):
-            post = a
-        elif o in ("-s", "--src"):
-            src = a        
-        elif o in ("-g", "--get"):
-            get = a
-        else:
-            print "unknown option: " + o
-            usage()
-            return 1
-        
-    file = open('/home/dilawar/.bloggerrc', 'r');
-    for line in file :
-      line = line.split('=')
-      key = line[0]
-      value = line[1]
-      if key.strip() == "blog" :
-        blog = value.strip()
-      elif key.strip() == "user" :
-        user = value.strip()
-      elif key.strip() == "password" :
-        password = value.strip()
-      else :
-        print key, "Unknown option"
-        return 1
+  # Getting command line arguments   
+  if not os.path.exists('/home/dilawar/.bloggerrc') :
+    print("""Create a ~/.bloggerrc file with following lines. "
+      blog=Dilawar's Blog
+      user=username
+      password=password
+      """)
+    sys.exit()
 
-    # Creating instance of BloggerUpdater
-    updater = BloggerUpdater.BloggerUpdater(user, password)
-    # Getting blog entry
-    blogEntry = updater.GetBlogByTitle(blog)
-    if blogEntry is None:
-       print "[E] Unable to find requested blog: \"" + blog + "\""
-       return 1
-    print "[I] Requested blog found: \"" + blogEntry.title.text + "\""
+  file = open('/home/dilawar/.bloggerrc', 'r');
+  for line in file :
+    line = line.split('=')
+    key = line[0]
+    value = line[1]
+    if key.strip() == "blog" :
+      blog = value.strip()
+    elif key.strip() == "user" :
+      user = value.strip()
+    elif key.strip() == "password" :
+      password = value.strip()
+    else :
+      print key, "Unknown option"
+      return 1
 
-    if get :
-      print("Fetching the post : {0}".format(get))
-      post = updater.GetPostByTitle(get)
-      if post :
-        fileName = (post.title.text).strip()
-        fileName = re.sub(r"\s+", "", fileName)
-        f = open(fileName+".html", 'w')
-        f.write(post.content.text)
-        f.close()
-        return
-      else :
-        print "I can't find post named "+get
+  # Creating instance of BloggerUpdater
+  updater = BloggerUpdater.BloggerUpdater(user, password)
+  # Getting blog entry
+  blogEntry = updater.GetBlogByTitle(blog)
+  if blogEntry is None:
+     print "[E] Unable to find requested blog: \"" + blog + "\""
+     return 1
+  print "[I] Requested blog found: \"" + blogEntry.title.text + "\""
+
+  if args.fetch :
+    print("Fetching the post : {0}".format(args.fetch))
+    post = updater.GetPostByTitle(args.fetch)
+    if post :
+      filename = (post.title.text).strip()
+      filename = filename.replace(" ", "_")
+      with open(filename+".html", 'w') as blog :
+        blog.write("<title>\n")
+        blog.write(post.title.text+"\n</title>")
+        blog.write("\n\n")
+        blog.write("<content>\n\n");
+        blog.write(post.content.text)
+        blog.write("\n\n</content>\n")
+      return
+    else :
+      print "I can't find post named "+get
+ 
+  elif args.send :  
+    file = args.send
+    if not os.path.exits(file) :
+      print("File {0} does not exists".format(src))
+      return
+    txt = open(src, 'r').read()   # Opening source HTML for reading
+    titleRegex = re.compile(r'\<title\>(?P<title>.+)\<\/title\>'
+        , re.IGNORECASE | re.DOTALL)
+    s = titleRegex.search(txt)
+    if not s :
+      print("I can not find the title. Existing ...")
+      return 
+      
+    title = m.groupdict()['title']
+    contentRegex = re.compile(r'\<content\>(?P<content>.+)\<\/content\>',
+        re.IGNORECASE | re.DOTALL)
+    s = contentRegex.search(txt)
+    if not s :
+      print("I can't find the content. Existing ...")
+      return 
+    content1 = s.groupdict()['content']
+    # Removing whitespace between HTML tags    
+    content1 = re.sub(r'>\s+<', "><", content1)    
+    # Removing leading and trailing whitespace
+    content = re.sub(r"[\n]+", "", content1)  
    
-    elif src :  
-      content1 = open(src, 'r').read()              # Opening source HTML for reading
-      content1 = re.sub(r'>\s+<', "><", content1)    # Removing whitespace between HTML tags
-      content = re.sub(r"[\n]+", "", content1)  # Removing leading and trailing whitespace
-     
-            # Getting post entry
-      postEntry = updater.GetPostByTitle(post)
-      if postEntry is None:
-          print "[E] Unable to find requested post: \"" + post + "\""
-          return 1
-      print "[I] Requested post found: \"%s\". Last update: %s. Updating ..." \
-          % (postEntry.title.text, postEntry.updated.text)
-      
-      # Updating post with new content
-      resultEntry = updater.UpdatePost(postEntry, content)
-      print "[I] Successfully updated: \"%s\". Last update: %s. Done!" \
-          % (resultEntry.title.text, resultEntry.updated.text) 
-      
-      return 0
-   
+    # Getting post entry
+    title = title.replace("\n","")
+    postEntry = updater.GetPostByTitle(title)
+    if postEntry is None:
+        print "[E] Unable to find requested post: \"" + post + "\""
+        return 1
+    print("[I] Requested post found: {0}. Last update: {1}. Updating ..." \
+        .format(postEntry.title.text, postEntry.updated.text))
+    
+    # Updating post with new content
+    resultEntry = updater.UpdatePost(postEntry, content)
+    print("[I] Successfully updated: {0}. Last update: {1}. Done!"\
+        .format(resultEntry.title.text, resultEntry.updated.text)) 
+    return 0
+ 
    
 if __name__ == '__main__':
-    main()
-    sys.exit(0)
+  parser = argparse.ArgumentParser(description="Blogger client")
+  parser.add_argument('--fetch', metavar="f"
+      , help="Fetch a post with similar looking name."
+      )
+  parser.add_argument('--send', metavar='s'
+      , help="Update a post."
+      )
+  args = parser.parse_args()
+  main(args)
