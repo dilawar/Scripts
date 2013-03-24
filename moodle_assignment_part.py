423adfbba46586ed136aed948775abd75aa9acff
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-24 07:15:34 +0530

    First extract the zip file and then extract

--- moodle_assignment_part.py
+++ moodle_assignment_part.py
@@ -5,6 +5,7 @@ import tarfile, zipfile
 import os, shutil, subprocess
 import glob
 from collections import defaultdict
+import errno
 
 
 
@@ -13,53 +14,57 @@ curDir = os.getcwd()
 
 assignments = defaultdict(list)
 
-def extract_asssignments(dir):
-        path = dir
-        os.chdir(path)
-        listing = glob.glob(path+'/*gz')
-        for file in listing:
-            print " |- Extracting archive ...{0}".format(file)
-            subprocess.call(["tar", "xzvf", file], stdout=subprocess.PIPE)
+def extract_asssignments(dirs):
+  for dir in dirs :
+    path = dir
+    os.chdir(path)
+    listing = glob.glob(path+'/*gz')
+    for file in listing:
+      print " |- Extracting archive ...{0}".format(file)
+      subprocess.call(["tar", "xzvf", file], stdout=subprocess.PIPE)
 
-        listing = glob.glob(path+'/*bz')
-        for file in listing:
-            print " |- Extracting archive ...{0}".format(file)
-            subprocess.call(["tar", "xjvf", file], stdout=subprocess.PIPE)
+    listing = glob.glob(path+'/*bz')
+    for file in listing:
+      print " |- Extracting archive ...{0}".format(file)
+      subprocess.call(["tar", "xjvf", file], stdout=subprocess.PIPE)
 
-        listing = glob.glob(path+'/*zip')
-        for file in listing:
-            print " |- Extracting archive ...{0}".format(file)
-            subprocess.call(["unzip", "-o", file], stdout=subprocess.PIPE)
+    listing = glob.glob(path+'/*zip')
+    for file in listing:
+      print " |- Extracting archive ...{0}".format(file)
+      subprocess.call(["unzip", "-o", file], stdout=subprocess.PIPE)
 
-        listing = glob.glob(path+'/*rar')
-        for file in listing:
-            print " |- Extracting archive ...{0}".format(file)
-            subprocess.call(["unrar", "x", "-o+", file], stdout=subprocess.PIPE)
+    listing = glob.glob(path+'/*rar')
+    for file in listing:
+      print " |- Extracting archive ...{0}".format(file)
+      subprocess.call(["unrar", "x", "-o+", file], stdout=subprocess.PIPE)
 
-        listing = glob.glob(path+'/*tar')
-        for file in listing:
-            print " |- Extracting archive ...{0}".format(file)
-            subprocess.call(["tar", "xvf", file], stdout=subprocess.PIPE)
+    listing = glob.glob(path+'/*tar')
+    for file in listing:
+      print " |- Extracting archive ...{0}".format(file)
+      subprocess.call(["tar", "xvf", file], stdout=subprocess.PIPE)
 
 
 
 with zipfile.ZipFile(inputFile, "r") as myzip :
-    listobj = myzip.infolist()
-    for obj in listobj :
-        zippedFile = obj.filename
-        filename = zippedFile.split("_")
-        studentName = filename[0].strip()
-        file = filename[1]
-        assignments[studentName].append(file)
+  listobj = myzip.infolist()
+  filesToExtract = list()
+  print("|- Extracting from zip file.")
+  for obj in listobj :
+    zippedFile = obj.filename
+    filename = zippedFile.split("_")
+    studentName = filename[0].strip()
+    file = filename[1]
+    assignments[studentName].append(file)
 
-        path = curDir+"/"+studentName
-        if os.path.exists(path) :
-            shutil.rmtree(path)
-            os.mkdir(path)
-        else:
-            os.mkdir(path)
-        myzip.extract(zippedFile, path)
-        pathToExtract= curDir+"/"+studentName+"/"+zippedFile
-        os.chdir(path)
-        extract_asssignments(path)
-        os.chdir(curDir)
+    path = curDir+"/"+studentName
+    
+    try :
+      os.makedirs(path)
+    except OSError as exception :
+      if exception.errno != errno.EEXIST :
+        raise 
+
+    myzip.extract(zippedFile, path)
+    pathToExtract= curDir+"/"+studentName+"/"+zippedFile
+    filesToExtract.append(path)
+  extract_asssignments(filesToExtract)
