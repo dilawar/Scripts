3e2a7e9a5a435a5221b19c5197573376425b3da1
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-04-19 04:13:49 +0530

    udpated

--- mutt/create_alias.sh
+++ mutt/create_alias.sh
@@ -3,7 +3,9 @@ MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
 
-if grep -Fxq "$NEWALIAS" $HOME/.mutt/aliases; then
+if grep -Fxq "moodle" "$NEWALIAS"; then
+  :
+elif grep -Fxq "$NEWALIAS" $HOME/.mutt/aliases; then
     :
 else
     echo "$NEWALIAS" >> $HOME/.mutt/aliases  
