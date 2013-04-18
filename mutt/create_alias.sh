8ef0ad7b735a5fc892652d352331385934ff448b
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-04-19 04:04:17 +0530

    updated

--- mutt/create_alias.sh
+++ mutt/create_alias.sh
@@ -3,7 +3,7 @@ MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
 
-if grep -Fxq "$NEWALIAS" $HOME/.mutt/aliases.txt; then
+if grep -Fxq "$NEWALIAS" $HOME/.mutt/aliases; then
     :
 else
     echo "$NEWALIAS" >> $HOME/.mutt/aliases  
