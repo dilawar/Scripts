022d180b1522d1ae91d833c18bbb6ec391aef17d
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-08-07 23:53:14 +0800

    updated xdefaults

--- easy_install.sh
+++ easy_install.sh
@@ -3,5 +3,7 @@
   polipo -c ~/.polipo &
 )
 export http_proxy=http://localhost:8123
+export https_proxy=http://localhost:8123
+export ftp_proxy=http://localhost:8123
 easy_install "$@"
 pkill polipo
