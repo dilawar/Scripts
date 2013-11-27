96e67535e4989722d98b22d22da1882b66c34a0a
Dilawar <dilawar.s.rajput@gmail.com>
2013-11-27 15:32:30 +0530

    Searching and adding to mpc

--- mpc_search_add.sh
+++ mpc_search_add.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-mpc search any $1 | xargs -I file mpc add file
+currentList=$(mpc -f %file% playlist)
+searched=$(mpc search any $1)
+IFS=$'\n'
+for song in $searched 
+do 
+    alreadyAdded="false"
+    for current in $currentList 
+    do
+        if [ "$song" == "$current" ]; then
+            echo "$song Already added. Ignoring"
+            alreadyAdded="true"
+            break
+        fi
+    done
+    
+    if [ "$alreadyAdded" == "false" ]; then
+        mpc add $song 
+    fi
+done
