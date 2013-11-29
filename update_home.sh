2fd2b697a1f4f45168254b0799278e28b29ace92
Dilawar <dilawars@iitb.ac.in>
2013-11-29 11:27:47 +0530

    updated updatehome dir

--- update_home.sh
+++ update_home.sh
@@ -14,7 +14,7 @@ cp $SCRIPTHOME/gitconfig $HOME/.gitconfig
 rm -f $HOME/.gitignore
 cp $SCRIPTHOME/gitignore $HOME/.gitignore 
 
-echo "Setting up mutt"
+echo "Setting up mutt ..."
 MUTTDIR=$HOME/.mutt
 if [ -d $MUTTDIR ]; then
     cd $MUTTDIR && git pull &&  cd
@@ -22,19 +22,44 @@ else
     git clone git@github.com:dilawar:mutt $MUTTDIR
 fi
 
-echo "Updating vim ..."
-VIMDIR=$HOME/.vim
-if [ -d $VIMDIR ]; then 
-    (
-        cd $VIMDIR && git pull && git submodule init && git submodule update 
-        rm -f $HOME/.vimrc
-        cp $VIMDIR/vimrc $HOME/.vimrc
-    )
-else 
-    (
-        git clone -b pathogen git@github.com:dilawar/vim $VIMDIR
-        cd $VIMDIR && git submodule init && git submodule update 
-    )
+echo "Updating awesome ... "
+AWESOMEDIR=$HOME/.config/awesome 
+if [ -d $AWESOMEDIR ]; then
+    cd $AWESOMEDIR && git pull 
+    echo "[INFO] Resetting awesome..."
+    xdotool key Super_L+ctrl+r
+else
+    git clone git@github.com:dilawar/awesome $AWESOMEDIR
+    echo "[TODO] Logout and login using awesome ..."
+fi
+
+echo "Setting up MPD ..."
+rm -f $HOME/.mpdconf
+cp $SCRIPTHOME/mpd/mpdconf $HOME/.mpdconf
+MPDHOME=$HOME/.mpd
+if [ -d $MPDHOME ]; then
+    echo "$MPDHOME exists .. Nothing to do."
+else
+    mkdir $MPDHOME 
+    mkdir $MPDHOME/playlists 
+fi
+
+echo "Setting up xfce4-terminal terminalrc ..."
+XFCE4HOME=$HOME/.config/xfce4/terminal
+if [ -d $XFCE4HOME ]; then
+    rm -f $XFCE4HOME/terminalrc
+    ln $SCRIPTHOME/terminalrc $XFCE4HOME/terminalrc
 fi
 
 
+echo "Updating vim ..."
+VIMDIR=$HOME/.vim
+if [ -d $VIMDIR ]; then 
+    cd $VIMDIR && git pull && git submodule init && git submodule update && cd
+    rm -f $HOME/.vimrc
+    ln $VIMDIR/vimrc $HOME/.vimrc
+else 
+    git clone -b pathogen git@github.com:dilawar/vim $VIMDIR
+    cd $VIMDIR && git submodule init && git submodule update && cd
+fi
+echo "[TODO] Open vim and run BundleInstall etc."
