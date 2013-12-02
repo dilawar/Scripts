2eca9a5f69487f1c2856f38999ef3d98193ee76f
Dilawar <dilawars@iitb.ac.in>
2013-12-02 15:21:27 +0530

    Added colored output shell scripts

--- update_home.sh
+++ update_home.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Run this script after editing any script in this directory.
 
+# Run this script after editing any script in this directory.
 if [ "$1" == "clean" ]; then
-    echo "Cleaning up everything ... "
+    colorPrint "STEP" "Cleaning up everything ... "
     rm -rf $HOME/.vim
     rm -rf $HOME/.mutt
     rm -rf $HOME/.config/awesome
@@ -10,18 +10,19 @@ if [ "$1" == "clean" ]; then
 fi
 
 SCRIPTHOME=$HOME/Scripts
+source $SCRIPTHOME/colors.sh
 
-echo "Setting up Xdefaults .."
+colorPrint "STEP" "Setting up Xdefaults .."
 rm -f $HOME/.Xresources
 rm -f $HOME/.Xdefaults
 ln $SCRIPTHOME/xdefaults $HOME/.Xresources
 ln $SCRIPTHOME/xdefaults $HOME/.Xdefaults
 
-echo "Setting mailcap"
+colorPrint "STEP" "Setting mailcap"
 rm -f $HOME/.mailcap
 ln $SCRIPTHOME/mailcap $HOME/.mailcap
 
-echo "Setting up urxvt terminal..."
+colorPrint "STEP" "Setting up urxvt terminal..."
 RXVTEXT=$HOME/.urxvt/ext
 if [ ! -d $RXVTEXT ]; then
     mkdir -p $RXVTEXT 
@@ -34,32 +35,32 @@ if [ ! -f $RXVTEXT/font-size ]; then
     && cd 
 fi
 
-echo "Setting up dzen and conky"
+colorPrint "STEP" "Setting up dzen and conky"
 if [[ $(which conky) == *"conky"* ]]; then
     rm -f $HOME/.conkyrc 
     ln $SCRIPTHOME/conkyrc $HOME/.conkyrc
 else
-    echo "[WARN] No conky found. Install and continue ..."
+    colorPrint "WARN" "No conky found. Install and continue ..."
     ln $SCRIPTHOME/dmenu_conky $HOME/Startup/dmenu_conky
 fi
 
-echo "Updating screenrc"
+colorPrint "STEP" "Updating screenrc"
 rm -f $HOME/.screenrc
 ln $SCRIPTHOME/screenrc $HOME/.screenrc 
 
 # Update bash 
-echo "Updating bash"
+colorPrint "STEP" "Updating bash"
 rm -f $HOME/.bashrc
 cp $SCRIPTHOME/bashrc $HOME/.bashrc 
 source $HOME/.bashrc 
 
-echo "Setting up git"
+colorPrint "STEP"  "Setting up git"
 rm -f $HOME/.gitconfig 
 cp $SCRIPTHOME/gitconfig $HOME/.gitconfig
 rm -f $HOME/.gitignore
 cp $SCRIPTHOME/gitignore $HOME/.gitignore 
 
-echo "Setting up mutt ..."
+colorPrint "STEP" "Setting up mutt ..."
 MUTTDIR=$HOME/.mutt
 if [ -d $MUTTDIR ]; then
     cd $MUTTDIR && git pull &&  cd
@@ -67,30 +68,30 @@ else
     git clone git@github.com:dilawar:mutt $MUTTDIR
 fi
 
-echo "Updating awesome ... "
+colorPrint "STEP" "Updating awesome ... "
 AWESOMEDIR=$HOME/.config/awesome 
 if [ -d $AWESOMEDIR ]; then
     cd $AWESOMEDIR && git pull 
-    echo "[INFO] Resetting awesome..."
+    colorPrint "STEP" "[INFO] Resetting awesome..."
     xdotool key Super_L+ctrl+r
 else
     git clone git@github.com:dilawar/awesome $AWESOMEDIR
-    echo "[TODO] Logout and login using awesome ..."
+    colorPrint "STEP" "[TODO] Logout and login using awesome ..."
 fi
 
-echo "Setting up MPD ..."
+colorPrint "STEP" "Setting up MPD ..."
 rm -f $HOME/.mpdconf
 cp $SCRIPTHOME/mpd/mpdconf $HOME/.mpdconf
 MPDHOME=$HOME/.mpd
 if [ -d $MPDHOME ]; then
-    echo "$MPDHOME exists .. Nothing to do."
+    colorPrint "STEP" "$MPDHOME exists .. Nothing to do."
 else
     mkdir $MPDHOME 
     mkdir $MPDHOME/playlists 
     touch $MPDHOME/tag_cache 
 fi
 
-echo "Setting up xfce4-terminal terminalrc ..."
+colorPrint "STEP" "Setting up xfce4-terminal terminalrc ..."
 XFCE4HOME=$HOME/.config/xfce4/terminal
 if [ -d $XFCE4HOME ]; then
     rm -f $XFCE4HOME/terminalrc
@@ -98,9 +99,9 @@ if [ -d $XFCE4HOME ]; then
 fi
 
 
-echo "Updating vim ..."
+colorPrint "STEP" "Updating vim ..."
 if [ ! -d $HOME/.backup ]; then
-    echo " + Creating backup git dir... "
+    colorPrint "STEP" " + Creating backup git dir... "
     mkdir $HOME/.backup 
     cd $HOME/.backup && git init 
 fi
@@ -114,4 +115,5 @@ else
     git clone -b pathogen git@github.com:dilawar/vim $VIMDIR
     cd $VIMDIR && git submodule init && git submodule update && cd
 fi
-echo "[TODO] Open vim and run BundleInstall etc."
+
+colorPrint "TODO" "Open vim and run BundleInstall etc."
