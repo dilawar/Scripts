2b8cacbbf1431c2a8838469b16084eae5e936dec
Dilawar <dilawars@iitb.ac.in>
2013-12-14 17:07:33 +0000

    Auto :

--- to_iitb.sh
+++ to_iitb.sh
@@ -12,13 +12,13 @@ if [ ! $(which $netcat) ]; then
 fi
 p_status=`$netcat -z $host $port; echo $?`
 echo "Status, $p_status"
-if [ $p_status == "1" ]; then
+if [[ "$p_status" == "1" ]]; then
     sshpass -pextvoxac ssh -t -t -D 5050 secure@login.iitb.ac.in -p 5022 > /dev/null &
 fi
 
-while [ $p_status == "0" ]; do
+while [[ "$p_status" == "0" ]]; do
     echo "Waiting for port to open...."
     p_status=`$netcat -z -w2 $host $port; echo $?`
 done
 echo "Port open. Making connection..."
-ssh -o ProxyCommand='$netcat -X 5 -x $host:$port %h %p' sharada.ee.iitb.ac.in -ldilawar
+ssh -o ProxyCommand="$netcat -X 5 -x $host:$port %h %p" sharada.ee.iitb.ac.in -ldilawar
