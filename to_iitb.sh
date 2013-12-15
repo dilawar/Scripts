24562b399f123ec6fff2f7d18c52f8246a63258a
Dilawar <dilawars@iitb.ac.in>
2013-12-15 05:51:32 +0000

    Updated script

--- to_iitb.sh
+++ to_iitb.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
-# Create a terminal and create a tunnel
+# This script creates a connection to iitb network from outside. Procedure is
+# suggested on this thread.
+# https://groups.google.com/forum/#!topic/wncc_iitb/5hLDnR38iy8
+#
+# Local host port and tunnel.
 port="5050"
 host="127.0.0.1"
+
 # On some platform nc-traditional is installed as default netcat. We are using
 # netcat-openbsd.
 netcat="nc.openbsd"
@@ -12,14 +16,17 @@ if [ ! $(which $netcat) ]; then
     exit
 fi
 p_status=`$netcat -z $host $port; echo $?`
-echo "Status, $p_status"
+
+# If port is not open, create a tunnel.
 if [[ "$p_status" == "1" ]]; then
     sshpass -pextvoxac ssh -t -t -D 5050 secure@login.iitb.ac.in -p 5022 > /dev/null &
 fi
 
+printf "Waiting for port to open "
 while [[ "$p_status" == "1" ]]; do
-    echo "Waiting for port to open...."
+    printf "."
     p_status=`$netcat -z -w2 $host $port; echo $?`
 done
-echo "Port open. Making connection..."
+printf ".. port is open!\n"
+echo "[INFO] Making connection"
 ssh -o ProxyCommand="$netcat -X 5 -x $host:$port %h %p" sharada.ee.iitb.ac.in -ldilawar
