792aeaddeeae2d508f75f918af66630d146704f8
Dilawar <dilawars@iitb.ac.in>
2013-12-16 17:58:47 +0000

    Added mairixrc:

--- moose_clone.sh
+++ moose_clone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 moose_url=svn+ssh://dilawars@svn.code.sf.net/p/moose/code
+multi_url=svn://svn.code.sf.net/p/moose/code/moose/branches/buildQ/python/moose
 
 if [ $1 == "git" ]; then
     if [ ! $2 ]; then
@@ -9,6 +10,9 @@ if [ $1 == "git" ]; then
         echo "Cloning from revision $2" 
         git svn clone -s -r$2:HEAD $moose_url moose_git_shallow
     fi
+elif [ $1 == "multiscale" ]; then
+    echo "git svn clone $multi_url"
+    git svn clone $multi_url moose_multiscale_git_svn
 else
     svn co $moose_url $1
 fi
