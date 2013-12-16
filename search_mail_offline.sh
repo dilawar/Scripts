66c4359542b4de76249837cfac5de08d1dc92a3d
Dilawar <dilawars@iitb.ac.in>
2013-12-16 18:37:50 +0000

    Auto : Updated moose link

--- search_mail_offline.sh
+++ search_mail_offline.sh
@@ -2,5 +2,5 @@
 
 # This script uses mairix to search and index and then mutt to browse those
 # emails. We assume that mairix has been configured beforehand.
-mairix $1
+mairix $@
 mutt -f ~/Mail/mfolder
