4fac669ee0e637dcee93a91681f43f9f4bcea858
Dilawar <dilawars@iitb.ac.in>
2013-12-19 16:00:55 +0000

    Play a song with matching query in mpc

---
+++ play.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_HOME=$HOME/Scripts
+source $SCRIPT_HOME/colors.sh
+
+currentList=$(mpc -f "[%position%]-[%file%]" playlist)
+demanded=$1
+colorPrint "INFO" "Searching for" $1
+IFS=$'\n'
+shopt -s nocasematch
+for current in $currentList 
+do
+    echo $current | grep $demanded
+    if [[ $current == *"$demanded"* ]]; then
+        position=${current%%-*}
+        colorPrint "INFO" "A matching song is found in playlist at $position"
+        mpc play $position
+        break
+    fi
+done
+colorPrint "INFO" "No song found for your query." 
