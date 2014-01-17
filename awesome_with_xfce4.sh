a5d381c9baea7343476e9723f339d584ff735d14
Dilawar <dilawars@iitb.ac.in>
2014-01-17 12:38:43 +0530

    fixed some stupid error in sniff

--- awesome_with_xfce4.sh
+++ awesome_with_xfce4.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -s "awesome" -a
+xfconf-query -c xfce4-session -p /sessions/Failsafe/Client0_Command -t string -s "killall xfce4 && awesome" -a
