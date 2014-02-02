88f184dff730bd6cc90473eb3302c6f74929a166
Dilawar <dilawars@iitb.ac.in>
2014-02-02 23:38:02 +0530

    Added a script which select HEAD only

---
+++ git_select_head.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python  
+import re
+import os 
+import sys
+
+pat = re.compile(r'\<{5,7}\s+HEAD\n(.+?)\={5,7}\n(.+?)\>{5,7}\s\w{39,40}'
+        , re.IGNORECASE | re.DOTALL
+        )
+
+def selectHead(txt):
+    global pat
+    txt = pat.sub('\1', txt)
+    return txt
+
+def selectRev(txt):
+    global pat 
+    txt = pat.sub('\2', txt)
+    return txt
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    if not os.path.isfile(filename):
+        print("Path does not exists")
+        sys.exit(0)
+    txt = open(filename, "r").read()
+    newTxt = selectHead(txt)
+    with open(filename, "w") as f:
+        f.write(newTxt)
+
+
