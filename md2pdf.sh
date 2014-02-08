7d3c6a1c1580a57db36921d887620992ac2cb4b6
Dilawar <dilawars@iitb.ac.in>
2014-02-08 23:02:53 +0530

    No non-default stuff

--- md2pdf.sh
+++ md2pdf.sh
@@ -14,13 +14,12 @@ fi
 texFile=${filename%.markdown}.tex
 texFile=${filename%.pandoc}.tex
 # now convert the file to pdf
-PANDOC="pandoc -V geometry:margin=15mm --data-dir=$HOME/Scripts/pandoc"
+PANDOC="pandoc --data-dir=$HOME/Scripts/pandoc"
 echo "Converting $filename to $outputFile using pandoc"
 latex="true"
 if [[ $latex = "true" ]]; then
     $PANDOC -s -f markdown+tex_math_dollars+latex_macros -t latex -o $texFile $filename
     pdflatex -shell-escape $texFile
-    rm -f *.log *.out *.aux
 else
     $PANDOC -s -f markdown+tex_math_dollars+latex_macros -o $outputFile $filename
 fi
