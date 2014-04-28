1b7d408b68e15e4812d3c142389b39e0029ec2d2
Dilawar <dilawars@iitb.ac.in>
2014-04-28 14:22:28 +0530

    By default binary files are not compared

--- diff_dir.py
+++ diff_dir.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python
+''' 
+This script compares two directories.
 
+Launch vimdiff if there are moderate changes in two files with same name.
+'''
 import difflib
 import os
 import sys
 import argparse
-import re
-import copy
 import subprocess
+
+from __future__ import division
+import string 
+
+def istext(filename):
+    ''' This function is from 
+    http://stackoverflow.com/questions/1446549/how-to-identify-binary-and-text-files-using-python
+    '''
+    s=open(filename).read(512)
+    text_characters = "".join(map(chr, range(32, 127)) + list("\n\r\t\b"))
+    _null_trans = string.maketrans("", "")
+    if not s:
+        # Empty files are considered text
+        return True
+    if "\0" in s:
+        # Files with null bytes are likely binary
+        return False
+    # Get the non-text characters (maps a character to itself then
+    # use the 'remove' option to get rid of the text characters.)
+    t = s.translate(_null_trans, text_characters)
+    # If more than 30% non-text characters, then
+    # this is considered a binary file
+    if len(t)/len(s) > 0.30:
+        return False
+    return True
 
 def insertIntoDict(fileName, dirName, dictName):
     """Insert a filename inside dirName into a dict
@@ -20,14 +47,25 @@ def insertIntoDict(fileName, dirName, dictName):
 def filterExtensions(dict, extensions):
     '''Remove files which are not in extensions.
     '''
+    global args
+
+    textFileDict = {}
+    if args.binary:
+        textFileDict = dict
+    else:
+        # Collect only binary files.
+        for f in dict:
+            if not istext(dict[f]):
+                textFileDict[f] = dict[f]
+
     if extensions is None:
         print("No extensions found")
         return dict
     newDict = {}
-    for f in dict:
+    for f in textFileDict:
         ext = f.split('.')[-1]
         if ext in extensions:
-            newDict[f] = dict[f]
+            newDict[f] = textFileDict[f]
         else:
             pass
     return newDict
@@ -113,6 +151,10 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--extension"
             , nargs = "*"
             , help = "Space separated list of extensions. Ignore all other files."
+            )
+    parser.add_argument("-b", "--enable_binary"
+            , default = False
+            , help = "Enable comparisions of binary files. Untested feature"
             )
     args = parser.parse_args()
     main()
