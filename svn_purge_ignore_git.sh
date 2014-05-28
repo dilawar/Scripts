e6cda00c742bd11f5af94ffc8bfa1dff478c9511
Dilawar <dilawars@iitb.ac.in>
2014-05-28 06:33:29 +0530

    Haddock will be open by elinks.

--- svn_purge_ignore_git.sh
+++ svn_purge_ignore_git.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #http://stackoverflow.com/questions/2803823/how-can-i-delete-all-unversioned-ignored-files-folders-in-my-working-copy
 # make sure this script exits with a non-zero return value if the
 # current directory is not in a svn working directory
@@ -12,7 +12,7 @@ while IFS= read -r f; do
     # instead of echo because different implementations of echo do
     # different things if the arguments begin with hyphens or
     # contain backslashes; the behavior of printf is consistent
-    if [[ "\.git" =~ "{f}" ]]; then
+    if [[ ".git" =~ "${f}" ]]; then
         printf '%s\n' "Deleting ${f}..."
         # if rm -rf can't delete the file, something is wrong so bail
         echo "Deleting ${f}"
