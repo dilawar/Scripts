4c7bbd8cbd1c36f1ec4839bd7574b01a370fc330
Dilawar <dilawars@ncbs.res.in>
2014-07-20 17:14:06 +0530

    updated script.

--- funtoo_mount.sh
+++ funtoo_mount.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 echo "Mouting filesystems"
-mount -t proc none proc
-mount --rbind /sys sys
-mount --rbind /dev dev
-echo "Chrooting into it"
-if [ ! -f etc/resolv.conf ]; then
-    cp /etc/resolv.conf etc
-fi
-env -i HOME=/root TERM=$TERM chroot . bash -l
+(
+    sudo mount -t proc none proc
+    sudo mount --rbind /sys sys
+    sudo mount --rbind /dev dev
+    echo "Chrooting into it"
+    if [ ! -f etc/resolv.conf ]; then
+        cp /etc/resolv.conf etc
+    fi
+    sudo env -i HOME=/root TERM=$TERM /usr/sbin/chroot . bash -l
+)
+export PS1="(chroot ) $PS1"
