8b67fabb9364625c3dc65114c9671943899aae35
Dilawar <dilawars@iitb.ac.in>
2014-08-29 15:18:26 +0530

    Move this to its own repository.

--- query_rcsb.py
+++ query_rcsb.py
@@ -31,19 +31,20 @@ logger.addHandler(h1)
 
 class Query():
 
-    def __init__(self, text, **kwargs):
+    def __init__(self, args, **kwargs):
         """Create a query with text"""
-        logger.info("Query for {}".format(text))
+        self.args = args
+        self.queryText = ' '.join(self.args.query)
+        logger.info("Querying for: {}".format(self.queryText))
         self.urlbase = 'http://www.rcsb.org/pdb/rest'
         self.url = self.urlbase + '/search'
-        self.text = text
         self.query = ET.Element("orgPdbQuery")
-        self.queryType = "org.pdb.query.simple.AdvancedKeywordQuery"
+        self.queryType = "org.pdb.query.simple." + args.query_type
         self.queryType = kwargs.get("queryType", self.queryType)
         self.addQueryType(self.queryType)
         self.ids = []
         if "AdvancedKeywordQuery" in self.queryType:
-            self.addKeywords(text)
+            self.addKeywords(self.queryText)
 
     def addKeywords(self, keywords):
         logger.info("Keywords: {}".format(keywords))
@@ -59,7 +60,10 @@ class Query():
 
     def addDescription(self):
         description = ET.SubElement(self.query, "description")
-        description.text = "Query type %s for text: %s" % (self.queryType, self.text)
+        description.text = "Query type %s for text: %s" % (
+                self.queryType
+                , self.queryText
+                )
 
     def getResults(self):
         """Fetch results"""
@@ -68,16 +72,20 @@ class Query():
         f = urllib2.urlopen(req)
         result = f.read()
         self.ids = filter(None, result.split("\n"))
-        
         logger.info("Total {} results are fetched".format(len(self.ids)))
         
     def getStructureReport(self, **kwargs):
         """Get information about an id"""
+        if not self.ids:
+            self.getResults()
+            if not self.ids:
+                print("Nothing found for query: %s" % self)
+            print self.ids
         size = kwargs.get('size', None)
         if not size:
-            size = min(1000, len(self.ids) - 1)
+            size = min(1000, len(self.ids))
 
-        logger.debug("Fetching: {} entries...".format(size))
+        logger.debug("Getting details of {} entries...".format(size+1))
         query = ",".join(self.ids[:size])
 
         format = kwargs.get('format', 'xml')
@@ -89,16 +97,34 @@ class Query():
         url += ',resolution,structureAuthor,classification'
         url += ',structureMolecularWeight,macromoleculeType'
         url += '&service={service}&format={format}'.format(service = service, format = format)
-        #logger.debug("Tyring: {}".format(url))
+        logger.debug("Report URL:\n {} \n".format(url))
         req = urllib2.Request(url)
         f = urllib2.urlopen(req)
-        return f.read()
+        self.report = f.read()
+
+def main():
+    import argparse
+    # Argument parser.
+    description = 'A script to download PDB files from RCSB Protein Data Bank'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--query', '-q', metavar='query'
+            , nargs = '+'
+            , required = True
+            , help = 'Query text'
+            )
+    parser.add_argument('--query_type', '-qt', metavar='queryType'
+            , default = 'AdvancedKeywordQuery'
+            , help = "Type of this query, prefixed by org.pdb.query.simple"
+            )
+    class Args: pass 
+    args = Args()
+    parser.parse_args(namespace=args)
+
+    # get the arguments.
+    q = Query(args)
+    q.getStructureReport()
+    print q.report
 
 if __name__ == "__main__":
-    text = " ".join(sys.argv[1:])
-    q = Query(text)
-    q.getResults()
-    if q.ids:
-        print q.getStructureReport(format = 'csv', size = 1000)
-    else:
-        print("Failed to get results")
+    main()
+
