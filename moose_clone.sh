77b50158131b2673d9bd240ff13e715967d32994
Dilawar Singh <dilawars@ncbs.res.in>
2014-10-09 10:36:29 +0530

    Updated links in moose-clone fiels.

--- moose_clone.sh
+++ moose_clone.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 moose_url=svn+ssh://dilawars@svn.code.sf.net/p/moose/code
-multi_url=svn+ssh://dilawars@svn.code.sf.net/p/moose/code/moose/branches/buildQ/python/moose
-async_url=svn+ssh://dilawars@svn.code.sf.net/p/moose/code/moose/branches/async_multiscale_cmake
-async13_url=svn+ssh://dilawars@svn.code.sf.net/p/moose/code/moose/branches/async13
+async_url=svn+ssh://dilawars@svn.code.sf.net/p/moose/code/moose/trunk
+alpha=svn+ssh://dilawars@svn.code.sf.net/p/moose/code/moose/pre3.0.0
 
 if [[ $1 = "git" ]]; then
     if [ ! $2 ]; then
@@ -12,16 +11,13 @@ if [[ $1 = "git" ]]; then
         echo "Cloning from revision $2" 
         git svn clone -s -r$2:HEAD $moose_url moose_git_shallow
     fi
-elif [[ $1 = "multiscale" ]]; then
-    echo "git svn clone $multi_url"
-    git svn clone $multi_url moose_multiscale_git_svn
+elif [[ $1 = "alpha" ]]; then
+    echo "git svn clone $async_url"
+    git svn clone $alpha moose_3.0.0_git_svn
 elif [[ $1 = "async" ]]; then
     echo "git svn clone $async_url"
-    git svn clone $async_url moose_async_multiscale_git_svn
-elif [[ $1 = "async13" ]]; then
-    echo "git svn clone $async13_url"
-    git svn clone $async13_url moose_async_git_svn
+    git svn clone $async_url moose_async_git_svn
 else
-    svn co --username=dilawar $moose_url $1
+    svn co --username=dilawars $moose_url $1
 fi
 
