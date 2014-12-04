d9bb364712b68d3bd9ec4cb4e2b4a49ceddbf793
Dilawar Singh <dilawars@ncbs.res.in>
2014-12-04 19:10:20 +0530

    This should clone the moose repository.

--- moose_clone.sh
+++ moose_clone.sh
@@ -2,22 +2,29 @@
 moose_url=https://dilawars@svn.code.sf.net/p/moose/code
 trunk=https://dilawars@svn.code.sf.net/p/moose/code/moose/trunk
 alpha=https://dilawars@svn.code.sf.net/p/moose/code/moose/pre3.0.0
+multiscale=https://dilawars@svn.code.sf.net/p/moose/code/moose/branches/async_multiscale_cmake
 
-if [[ $1 = "git" ]]; then
-    if [ ! $2 ]; then
-        echo "Cloning with all history..."
-        git svn clone $moose_url moose_git
-    else
-        echo "Cloning from revision $2" 
-        git svn clone -s -r$2:HEAD $moose_url moose_git_shallow
-    fi
-elif [[ $1 = "alpha" ]]; then
-    echo "git svn clone $alpha"
-    git svn clone $alpha moose_3.0.0_git_svn
-elif [[ $1 = "async" ]]; then
-    echo "git svn clone  $trunk"
-    git svn clone $trunk moose_async_git_svn
+CLONER=git svn
+if [[ $2 = "svn" ]]; then
+    echo "Using svn."
+    CLONER=svn co
 else
-    svn co --username=dilawars $moose_url $1
+    echo "Using git-svn"
+    CLONER="git svn clone"
 fi
 
+if [[ $1 = "git" ]]; then
+    echo "Cloning from $2 to HEAD"
+    git svn clone -r$2:HEAD $moose_url moose_git_shallow
+elif [[ $1 = "alpha" ]]; then
+    echo "$CLONER $alpha"
+    $CLONER clone $alpha moose_3.0.0_git_svn
+elif [[ $1 = "trunk" ]]; then
+    echo "$CLONER $trunk"
+    $CLONER $trunk moose_trunk_git
+elif
+    echo "$CLONER clone $multiscale"
+    $CLONER $multiscale moose_multiscale_git
+else
+    $CLONER --username=dilawars $moose_url $1
+fi
