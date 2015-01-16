b30159084ec73463e15479247152c67e0b586a4f
Dilawar Singh <dilawars@ncbs.res.in>
2015-01-16 23:39:09 +0530

    Auto : Using agrep in nls

--- notes.sh
+++ notes.sh
@@ -1,11 +1,20 @@
-NOTEDIR=notes
+NOTEDIR=$HOME/Work/notes
 EXT=txt
 n() {
     $EDITOR $NOTEDIR/"$*".$EXT
 }
 
+# Searches for notes with given pattern. Using agrep is recommened.
 nls() {
-    ls -c $NOTEDIR | grep "$*"
+    GREP=`which agrep`
+    if [ ! -f  $GREP ]; then
+        echo "I use $GREP. Please install it. Continuing with grep"
+        GREP=`which grep`
+    fi
+    if [[ "$GREP" == *"agrep"* ]]; then
+        GREP="$GREP -4"
+    fi
+    ( cd $NOTEDIR && $GREP -l "$*" *.$EXT )
 }
 
 nsync() {
