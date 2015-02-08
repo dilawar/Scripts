275c334f23b731f8a06c95f404349b56bc6606f2
Dilawar Singh <dilawars@ncbs.res.in>
2015-02-08 15:51:43 +0530

    Added markdown to ps file.

---
+++ markdown_to_ps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+# This script uses pandoc to convert pandoc to ps.. 
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "USAGE: ./$0 filename.pandoc [output.ps]"
+    exit
+fi
+
+LATEX="latex -shell-escape"
+if [ ! -d .temp ]; then
+    mkdir -p .temp
+fi
+
+filename=$1
+if [ $# -eq 2 ]; then
+    outputFile=$2
+else
+    outputFile="${filename%.pandoc}.ps"
+fi
+
+texFile=${filename%.pandoc}.tex
+# now convert the file to pdf
+PANDOC="pandoc --data-dir=$HOME/Scripts/pandoc --filter=pandoc-citeproc --number-sections"
+INFMT=markdown+tex_math_dollars+latex_macros+header_attributes+yaml_metadata_block+table_captions
+echo "Converting $filename to $outputFile using pandoc"
+latex="true"
+if [[ $latex = "true" ]]; then
+    cp *.pandoc .temp
+    (
+        cd .temp
+        $PANDOC -s -f $INFMT -t latex -o $texFile $filename
+        $LATEX  $texFile
+    )
+    mv .temp/*.ps .
+    cp .temp/*.tex .
+else
+    $PANDOC -s -f $INFMT -o $outputFile $filename
+fi
+
