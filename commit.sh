de6427bcd0dcf82a70c10029d56891539d7e2f1a
Dilawar Singh <dilawars@ncbs.res.in>
2015-02-19 00:01:25 +0530

    Pandoc is also added to default file to commit by this script.

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find . -type f -regex ".*\.\(sh\|py\|Make*\|lua\|config\|hs\|pl\|vhd\|v\|vim\)" -print0 | xargs -0 -I file git add file 
+find . -type f -regex ".*\.\(sh\|pandoc\|py\|Make*\|lua\|config\|hs\|pl\|vhd\|v\|vim\)" -print0 | xargs -0 -I file git add file 
 git commit -m "$1"
 git push 
