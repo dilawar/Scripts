b66602eec0edae7512e28536c706b9b8b0ad732f
Dilawar Singh <dilawars@ncbs.res.in>
2015-04-11 23:27:31 +0530

    Updated script to plot csv file.

--- plot_csv.py
+++ plot_csv.py
@@ -1,32 +1,54 @@
 #!/usr/bin/env python
 # A quick plotting of csv files.
-
 import sys
 import pylab
+import re
 import os
-from collections import defaultdict
+import numpy as np
 
 class Args: pass 
 args = Args()
 
-xvec = defaultdict(list)
-yvecs = defaultdict(list)
-
-def plotLines(header, lines):
-    xindex = args.xcolumn
-    yindices = args.ycolumns
-    print yindices
-    for l in lines:
-        l = l.split(",")
-
+def getHeader(filename):
+    d = args.delimiter
+    hline = args.header
+    if not hline:
+        return None
+    with open(filename, "r") as f:
+        header = f.read().split("\n")[0]
+    header = header.split(",")
+    args.header = header
+    return header
 
 def main(args):
-    with open(args.input_file, "r") as f:
-        txt = f.read()
-    lines = txt.split("\n")
-    lines = filter(None, [l.strip() for l in lines])
-    plotLines(lines[0], lines[1:])
+    if not args.ycolumns:
+        usecols = args.xcolumn 
+    else:
+        usecols = [ args.xcolumn ] + args.ycolumns 
 
+    header = getHeader(args.input_file)
+    if not header:
+        skiprows = 0
+    else:
+        skiprows = 1
+    data = np.loadtxt(args.input_file
+            , skiprows = skiprows
+            , delimiter = args.delimiter
+            , usecols = usecols
+            )
+    data = np.transpose(data)
+    xvec = data[0]
+    for i, d in enumerate(data[1:]):
+        pylab.plot(xvec, d, '.', label = args.header[i+1])
+        pylab.legend(loc='best', framealpha=0.4)
+    if args.header:
+        pylab.xlabel("%s" % args.header[0])
+
+    if not args.outfile:
+        pylab.show()
+    else:
+        print("[INFO] Saving figure to %s" % args.outfile)
+        pylab.savefig(args.outfile)
 
 if __name__ == '__main__':
     import argparse
@@ -34,8 +56,16 @@ if __name__ == '__main__':
     description = '''A csv file plotter'''
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--input_file', '-in', metavar='input csv file'
-            , required = True
-            , help = 'File to plot'
+        , required = True
+        , help = 'File to plot'
+        )
+    parser.add_argument('--delimiter', '-d'
+        , default = ','
+        , help = 'Delimiter'
+        )
+    parser.add_argument('--header', '-t'
+            , default = True
+            , help = "Is first line header?"
             )
     parser.add_argument('--xcolumn', '-x'
         , default =  0
@@ -43,9 +73,13 @@ if __name__ == '__main__':
         , help = 'Which column is x-axis'
         )
     parser.add_argument('--ycolumns', '-y'
-            , nargs = '+'
-            , type = int
-            , help = "Colums to plot as y-axis"
-            )
+        , nargs = '+'
+        , type = int
+        , help = "Colums to plot as y-axis"
+        )
+    parser.add_argument('--outfile', '-o'
+        , required = False
+        , help = 'Save plot as this file'
+        )
     parser.parse_args(namespace=args)
     main(args)
