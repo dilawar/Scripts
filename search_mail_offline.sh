d3a0c67902727423eddca9fd9b9036c087c9c6a8
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-27 17:17:51 +0530

    I should be able to create backup in git of new files.

--- search_mail_offline.sh
+++ search_mail_offline.sh
@@ -2,5 +2,18 @@
 
 # This script uses mairix to search and index and then mutt to browse those
 # emails. We assume that mairix has been configured beforehand.
-mairix $@
+MAIRIX=`which mairix`
+MAIRIX_DIR=$HOME/Mail/mfolder
+if [ ! $MAIRIX ]; then
+    echo "mairix is not found."
+    exit
+fi
+
+if [ ! -d $MAIRIX_DIR ]; then
+    echo "Did you setup mairix"
+    echo "See the man page of mairixrc"
+    mkdir -p $MAIRIX_DIR
+fi
+
+$MAIRIX $@
 mutt -f ~/Mail/mfolder
