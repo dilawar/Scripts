a8ad2f014d57c49b9aff340951ed66c9bd8b2d54
Dilawar Singh <dilawars@ncbs.res.in>
2015-08-01 18:48:21 +0530

    Script to filter branch in git.

--- git_purge_from_repo.py
+++ git_purge_from_repo.py
@@ -4,7 +4,9 @@ import subprocess
 import sys 
 import string
  
-usage = "Usage : purge_git_history.py [-f | -d] file_or_dir_name"
+
+usage = "Usage : {0} [-f | -d] file_or_dir_name".format(sys.argv[0])
+
 if len(sys.argv) < 3 :
   print usage
   sys.exit(0)
@@ -12,17 +14,19 @@ if len(sys.argv) < 3 :
 if sys.argv[1] == "-f" :
   filename = sys.argv[2]
   print("Purging file {0} ...".format(filename))
-  command = '''git filter-branch -f --index-filter 'git rm -f {0}' 
-    --tag-name-filter cat -- --all'''.format(filename)
+  command = """git filter-branch -f --index-filter 'git rm --cached
+  --ignore-unmatch {0}' --tag-name-filter cat -- --all""".format(filename)
+  print("Runing: %s" % command)
   command = string.replace(command, "\n", " ")
   subprocess.call(command, shell=True)
 
 elif sys.argv[1] == "-d" :
   dirname = sys.argv[2]
-  command = '''git filter-branch -f --tree-filter 'rm -rf {0}' 
+  command = '''git filter-branch -f --tree-filter 'git rm -rf --ignore-unmatch {0}' 
   --tag-name-filter cat -- --all'''.format(dirname)
   command = string.replace(command, "\n", " ")
   print("Purging directory {0}".format(dirname))
+  print("Running %s" % command)
   subprocess.call(command, shell=True)
 
 else :
@@ -34,5 +38,5 @@ else :
 subprocess.call("rm -rf ./.git/refs/original", shell=True)
 subprocess.call("git reflog expire --expire=now --all", shell=True)
 subprocess.call("git gc --prune=now", shell=True)
-subprocess.call("git push origin master --force", shell=True)
+#subprocess.call("git push origin master --force", shell=True)
 
