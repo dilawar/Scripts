1cec50be5ce54dc2cbc6ca71dc4440e638119fc7
Dilawar Singh <dilawars@ncbs.res.in>
2015-08-07 18:34:53 +0530

    Script to install ghi.

--- install_all.sh
+++ install_all.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
+source install/ghi.sh
 
-# this scripts install all essential packages on unix system. 
-# For personal use.
-(
-    echo "Installing ghi (github issues)"
-    curl -sL https://raw.githubusercontent.com/stephencelis/ghi/master/ghi > /tmp/ghi
-    chmod 755 /tmp/ghi
-    sudo mv /tmp/ghi /usr/local/bin/
-)
