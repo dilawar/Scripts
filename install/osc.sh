a331d33efb4519644d7ece9fb566e8c50c42f6bf
Dilawar Singh <dilawars@ncbs.res.in>
2015-08-11 22:54:33 +0530

    Install osc, TODO: tar_scm plugins.

--- install/osc.sh
+++ install/osc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-git clone https://github.com/openSUSE/osc.git /tmp
+git clone --depth 10 https://github.com/openSUSE/osc.git /tmp/osc.git
 (
     cd /tmp/osc.git
     python setup.py install --user
