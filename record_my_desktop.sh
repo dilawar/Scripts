d922dc9c63022cbc7f71330c80517ae01c55deba
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-11 12:36:14 +0530

    Don't scroll on output.

--- record_my_desktop.sh
+++ record_my_desktop.sh
@@ -6,4 +6,4 @@
 #rm -f ~/Video/output.mkv
 Xaxis=$(xrandr -q | grep '*' | uniq | awk '{print $1}' |  cut -d 'x' -f1)
 Yaxis=$(xrandr -q | grep '*' | uniq | awk '{print $1}' |  cut -d 'x' -f2)
-ffmpeg -f x11grab -s $(($Xaxis))x$(($Yaxis)) -r 25 -i :0.0 -sameq ~/Video/output.mkv
+avconv -f x11grab -s $(($Xaxis))x$(($Yaxis)) -r 25 -i :0.0 ~/Videos/output.mkv
