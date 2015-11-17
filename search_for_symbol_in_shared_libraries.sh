fddbc1e54d499c3a50557545149213f5539978ae
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-17 16:25:36 +0530

    no circuitikz by default.

--- search_for_symbol_in_shared_libraries.sh
+++ search_for_symbol_in_shared_libraries.sh
@@ -3,3 +3,5 @@ echo "Searching for $1 in /usr"
 find /usr -name "*.so" -exec nm --print-file-name --defined-only \
     --dynamic {} \; | grep "$1"
 
+find /lib -name "*.so" -exec nm --print-file-name --defined-only \
+    --dynamic {} \; | grep "$1"
