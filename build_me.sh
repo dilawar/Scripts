9b5cf96b2428710630b7cdef50f9ce112cffb024
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-22 01:59:11 +0530

    minor update

--- build_me.sh
+++ build_me.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-if [[ $1 == "-d" ]]; then
+## Build using cmake.
+if [[ $1 == "--clean" ]]; then
     echo "Cleaning old cmake cache files"
     find . -name "*CMakeCache*" -print0 | xargs -I file rm -f file
+    exit
 else
     echo "Warning: Using old CMakeCache files."
 fi
-cmake .
-make
+mkdir -p _build
+(
+    cd _build
+    cmake "$@" ..
+    make -j`nproc`
+)
