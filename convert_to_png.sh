a98bcec967738a5bbbadf89d7390ba65c59cf59d
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-26 12:13:29 +0530

    Can now convert pdf to png as well.

--- convert_to_png.sh
+++ convert_to_png.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+set -x
 
 # Convert any image file to png.
+
+function convert2png 
+{
+    FILE="$1"
+    OUTFILE=${FILE%\.*}.png
+    echo "[INFO] converting $FILE --> $OUTFILE"
+    convert $FILE $OUTFILE
+}
+
 
 for FILE in "$@"; do
     if file --mime-type $FILE | grep -i image/  
     then 
-        OUTFILE=${FILE%\.*}.png
-        echo "[INFO] converting $FILE --> $OUTFILE"
-        convert $FILE $OUTFILE
+        convert2png $FILE
+    elif file --mime-type $FILE | grep -i application/pdf 
+    then
+        convert2png $FILE
     fi
 done
