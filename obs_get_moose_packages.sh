1f7b80f1d82f52b199121e61ba0dc418c280c8a5
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-30 13:10:11 +0530

    Get both server version and moose.

--- obs_get_moose_packages.sh
+++ obs_get_moose_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Download all packages.
-set -e
+set -x
 REPOS="Debian_7.0 Debian_8.0 Fedora_22 Fedora_20 openSUSE_13.2\
     openSUSE_Leap_42.1 CentOS_7 RHEL_7 ScientificLinux_7"
 ARCHS="x86_64 i585"
@@ -8,5 +8,7 @@ for r in $REPOS; do
     for arch in $ARCHS; do
         echo "Downloading $r, $arch"
         osc getbinaries home:dilawar/moose/$r/$arch
+        osc getbinaries home:moose/moose-python/$r/$arch
     done
 done
+
