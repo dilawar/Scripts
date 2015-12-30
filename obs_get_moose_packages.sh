b9ffd5884bf0e68e4de38b71d1b1f563d2e13797
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-30 13:11:07 +0530

    Fixed typo.

--- obs_get_moose_packages.sh
+++ obs_get_moose_packages.sh
@@ -3,7 +3,7 @@
 set -x
 REPOS="Debian_7.0 Debian_8.0 Fedora_22 Fedora_20 openSUSE_13.2\
     openSUSE_Leap_42.1 CentOS_7 RHEL_7 ScientificLinux_7"
-ARCHS="x86_64 i585"
+ARCHS="x86_64 i586"
 for r in $REPOS; do
     for arch in $ARCHS; do
         echo "Downloading $r, $arch"
