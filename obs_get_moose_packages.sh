e6c8762a9d4d3b56008bcf863f3c6ff88be43c6b
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-31 09:22:33 +0530

    floating enabled for gimp and imagej.

--- obs_get_moose_packages.sh
+++ obs_get_moose_packages.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Download all packages.
 set -x
-REPOS="Debian_7.0 Debian_8.0 Fedora_22 Fedora_20 openSUSE_13.2\
-    openSUSE_Leap_42.1 CentOS_7 RHEL_7 ScientificLinux_7"
+REPOS="Debian_7.0 Debian_8.0 \
+    xUbuntu_12.04 xUbuntu_14.0 xUbuntu_15.05 xUbuntu_15.10 \
+    Fedora_22 Fedora_23 \
+    openSUSE_13.2 openSUSE_Leap_42.1 \
+    CentOS_7 RHEL_7 ScientificLinux_7"
+
 ARCHS="x86_64 i586"
 for r in $REPOS; do
     for arch in $ARCHS; do
