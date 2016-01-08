4d36dc5d896838763f5c6092ec1740e387353f5c
Dilawar Singh <dilawars@ncbs.res.in>
2016-01-08 16:36:08 +0530

    Also backup ~/.config and .firefox.

--- shares_backup.sh
+++ shares_backup.sh
@@ -3,7 +3,7 @@
 START=$(date +%s)
 outfile=/tmp/"Backup_$(date '+%A,%d%B%Y,%T')"
 sshpass -e rsync -ab --progress --delete --backup-dir=old_`date +%F` --delete \
-    --exclude=old_* ~/Work dilawars@shares.ncbs.res.in:~
+    --exclude=old_* ~/Work ~/.config ~/.firefox dilawars@shares.ncbs.res.in:~
 FINISH=$(date +%s)
 echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds"
 notify-send "Done backing up Work to shares.ncbs.res.in"
