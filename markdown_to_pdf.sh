3d3060612d370f37b3daaf491b7dd76fbeda3256
Dilawar Singh <dilawars@ncbs.res.in>
2016-03-08 15:06:16 +0530

    Download recursively.

--- markdown_to_pdf.sh
+++ markdown_to_pdf.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
+unset TEXMF
 # This script uses pandoc to convert markdown to pdf. 
 if [ $# -lt 1 ] || [ $# -gt 2 ]; then
     echo "USAGE: ./$0 filename.markdown [output.pdf]"
     exit
 fi
 
-LATEX="pdflatex -shell-escape"
+LATEX="lualatex -shell-escape"
 if [ ! -d .temp ]; then
     mkdir -p .temp
 fi
--- mirror_web.sh
+++ mirror_web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 echo "Mirroring $@"
-wget --no-clobber --mirror --no-parent --convert-links --adjust-extension \
+wget -r --no-clobber --mirror --no-parent --convert-links --adjust-extension \
     "$@"
