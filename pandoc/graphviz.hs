bd995e951703b50cc1e627de996306cd9df34b26
Dilawar Singh <dilawars@ncbs.res.in>
2016-08-08 13:31:11 +0530

    My pandoc filters.

---
+++ pandoc/graphviz.hs
@@ -0,0 +1,35 @@
+#!/usr/bin/env runhaskell
+import Text.Pandoc.JSON
+
+import System.Process
+
+import Control.Concurrent
+import Data.List
+
+main :: IO ()
+main = toJSONFilter insertDot
+
+-- | Poor mans function to get a file extension
+getFileExtension fileName = reverse ext
+  where
+    (ext, _) = break (=='.') . reverse $ fileName
+
+insertDot :: Block -> IO Block
+insertDot (CodeBlock (ident, ["showDot"] , attrs) code) = do
+    let Just (_,outFile) = find (\(key, value) -> key == "outFile") attrs
+    let ext = getFileExtension outFile
+
+    writeFile "tmp.dot" code
+    system $ "dot tmp.dot -T" ++ ext ++ " > " ++ outFile
+    return $ Para $ [Image [Str "test"] (outFile,"test title")]
+insertDot (CodeBlock (ident, ["showDotCode"] , attrs) code) = do
+    let Just (_,outFile) = find (\(key, value) -> key == "outFile") attrs
+    let ext = getFileExtension outFile
+
+    writeFile "tmp.dot" code
+    system $ "dot tmp.dot -T" ++ ext ++ " > " ++ outFile
+    return $ Div ("",[],[]) [(CodeBlock (ident, ["haskell"] , attrs) code)
+                            ,Para $ [Image [Str "test"] (outFile,"test title")]
+                            ]
+insertDot x = return x
+
