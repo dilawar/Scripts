6675f5310cdd7dd9a96463c51e223c856d895968
Dilawar Singh <dilawars@ncbs.res.in>
2016-09-16 16:01:05 +0530

    When done emit the path of file on shares.ncbs.res.in

--- shares_backup_file.sh
+++ shares_backup_file.sh
@@ -17,7 +17,6 @@
 #      REVISION:  ---
 #===============================================================================
 
-set -x
 set -o nounset                              # Treat unset variables as an error
 if [[ $# -lt 1 ]]; then
     echo "USAGE: $0 filepath"
@@ -25,4 +24,7 @@ if [[ $# -lt 1 ]]; then
 fi
 FILENAME="$1"
 sshpass -e rsync -azv --progress $FILENAME dilawars@shares.ncbs.res.in:~/archives/
+echo "Here is the path of the file on shares"
+sshpass -e ssh dilawars@shares.ncbs.res.in find /home/bhalla/dilawars \
+    -type f -name "*${FILENAME}"
 notify-send "Done backing up $FILENAME"
