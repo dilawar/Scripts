6394a731c5ed4d26cacd0b36b1636f75c8cb1cde
Dilawar Singh <dilawars@ncbs.res.in>
2016-09-23 12:58:30 +0530

    When everything else fails.

---
+++ ghc_pkg_clean.sh
@@ -0,0 +1,8 @@
+# unregister broken GHC packages. Run this a few times to resolve dependency rot in installed packages.                                                                                                                                     
+function ghc-pkg-clean() {
+    for p in `ghc-pkg check $* 2>&1  | grep problems | awk '{print $6}' | sed -e 's/:$//'`
+    do
+        echo unregistering $p; ghc-pkg $* unregister $p
+    done
+}
+ghc-pkg-clean
