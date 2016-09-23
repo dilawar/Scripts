872168030c621fb38d9946e3c76b2f2b041b2a78
Dilawar Singh <dilawars@ncbs.res.in>
2016-09-23 17:35:30 +0530

    restart udev.

--- ghc_pkg_clean.sh
+++ ghc_pkg_clean.sh
@@ -2,7 +2,7 @@
 function ghc-pkg-clean() {
     for p in `ghc-pkg check $* 2>&1  | grep problems | awk '{print $6}' | sed -e 's/:$//'`
     do
-        echo unregistering $p; ghc-pkg $* unregister $p
+        echo unregistering $p; ghc-pkg $* unregister --force $p
     done
 }
 ghc-pkg-clean
