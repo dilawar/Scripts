a0a905e63ee7135f8449f374397a26cff7f6c238
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-29 13:08:36 +0530

    cleanup local tree.

---
+++ svn_clean_fxd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: svn_clean_fxd.sh
+# 
+#         USAGE: ./svn_clean_fxd.sh 
+# 
+#   DESCRIPTION:  Like git -fxd
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: 03/29/2017 01:08:03 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+svn st | grep '^?' | awk '{print $2}' | xargs rm -rf
