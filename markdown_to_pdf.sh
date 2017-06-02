66d1949b4a1820bc37b6ecba1aef4e26b55c1e9f
Dilawar Singh <dilawars@ncbs.res.in>
2017-06-02 09:41:48 +0530

    Dont include filters.

--- markdown_to_pdf.sh
+++ markdown_to_pdf.sh
@@ -17,7 +17,7 @@ outputFile="${filename%.pandoc}.pdf"
 
 texFile=${filename%.pandoc}.tex
 # now convert the file to pdf
-PANDOC="pandoc ${@:2} --filter $HOME/Scripts/pandoc/image_local_copy.hs "
+PANDOC="pandoc ${@:2}" # --filter $HOME/Scripts/pandoc/image_local_copy.hs "
 PANDOC="$PANDOC --data-dir=$HOME/Scripts/pandoc --number-sections"
 
 INFMT=markdown+citations+tex_math_dollars+multiline_tables+latex_macros+header_attributes+yaml_metadata_block+table_captions
--- osc_build.sh
+++ osc_build.sh
@@ -9,9 +9,9 @@ fi
 if [[ "$1" == *"Debian"* ]]; then
     osc build --noservice $1 --no-verify $2.dsc | tee __build_$1.log
 elif [[ "$1" == *"Ubuntu"* ]]; then
-    osc build --noservice $1 --no-verify  $2.dsc | tee __build_$1.log
+    osc build --noservice $1 --no-verify $2.dsc | tee __build_$1.log
 else
-    osc build --noservice $1 $2.spec --no-verify | tee __build_$1.log
+    osc build --noservice $1 --no-verify $2.spec  | tee __build_$1.log
 fi
 
 
