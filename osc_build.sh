1ceecea04940d4d95dcac6047ebb584553a6c96e
Dilawar Singh <dilawars@ncbs.res.in>
2017-07-03 19:09:25 +0530

    Updated script.

--- osc_build.sh
+++ osc_build.sh
@@ -2,16 +2,17 @@
 # Build packaing using osc.
 set -x
 set -e
+PACKAGE_NAME=$(basename $(realpath .))
 echo "Building for repo $1 package $2"
-if [ ! -f ./_service:recompress:tar_scm:$2*.tar* ]; then
+if [ ! -f ./_service:recompress:tar_scm:${PACKAGE_NAME}*.tar* ]; then
     osc service run
 fi
 if [[ "$1" == *"Debian"* ]]; then
-    osc build --noservice $1 --no-verify $2.dsc | tee __build_$1.log
+    osc build --noservice $1 --no-verify $PACKAGE_NAME.dsc | tee __build_$1.log
 elif [[ "$1" == *"Ubuntu"* ]]; then
-    osc build --noservice $1 --no-verify $2.dsc | tee __build_$1.log
+    osc build --noservice $1 --no-verify $PACKAGE_NAME.dsc | tee __build_$1.log
 else
-    osc build --noservice $1 --no-verify $2.spec  | tee __build_$1.log
+    osc build --noservice $1 --no-verify $PACKAGE_NAME.spec  | tee __build_$1.log
 fi
 
 
