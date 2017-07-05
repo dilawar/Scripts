29fed84cd475783d53b55ede4392a8a1b7d6abe0
Anushree <anushreen@ncbs.res.in>
2017-07-05 14:12:11 +0530

    Added script to mount filesystem from srna to local machine

---
+++ mount_all_data_on_local_system.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sshfs anushreen@srna.ncbs.res.in:/home/shivaprasad/anushreen $HOME/SHARES
+sshfs anushree@srna.ncbs.res.in:/data/anushree $HOME/SRNA
+sshfs anushree@srna.ncbs.res.in:/data/shivaprasad/Anushree $HOME/DATA
