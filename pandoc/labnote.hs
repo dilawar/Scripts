89210488151bd6c793ba80c92f481c6585381197
Dilawar Singh <dilawars@ncbs.res.in>
2017-08-09 13:49:37 +0530

    My filters only.

--- pandoc/labnote.hs
+++ pandoc/labnote.hs
@@ -12,83 +12,6 @@ import System.IO
 import System.Directory
 
 
--- | Filter to handle commands.
-handleCmds :: Block -> IO Block
-handleCmds (CodeBlock (ident, [] , (("showCmdBlock",cmd0):attrs)) code) = do
-    cmdBlock <- handleCmds $ CodeBlock (ident, [], (("cmdBlock",cmd0):attrs)) code
-    return $ Div ("", ["groupCodeBlock"], [])
-                 [ CodeBlock (ident, ["bash"], []) $ "$ " <> cmd0
-                 , cmdBlock
-                 ]
-handleCmds (CodeBlock (ident, [] , (("cmdBlock",cmd0):attrs)) code) = do
-    let (cmd:args) = words cmd0
-    let outClasses =
-              words
-            . maybe [] snd
-            . find ((== "outClasses") . fst)
-            $ attrs
-    let inClasses =
-              words
-            . maybe [] snd
-            . find ((== "inClasses") . fst)
-            $ attrs
-    let inputBlock =
-            if null code
-            then Null
-            else CodeBlock (ident, inClasses, []) code
-    result <- readProcess cmd args code
-    return $ Div ("", ["groupCodeBlock"], [])
-                 [ inputBlock
-                 , CodeBlock (ident, outClasses, []) result
-                 ]
-handleCmds x = return x
-
--- | Filter to insert diagram.
-insertDiagrams :: Block -> IO Block
-insertDiagrams (CodeBlock (ident, ["diagram"] , attrs) code) = do
-    let Just (_,outFile) = find (\(key, value) -> key == "outFile") attrs
-    writeFile "tmp.hs" code
-    system "ghc -O2 tmp.hs -o tmp -rtsopts > /dev/null"
-    system "./tmp +RTS -K100M > /dev/null"
-    return $ Para $ [Image nullAttr [Str "test"] (outFile,"test title")]
-insertDiagrams (CodeBlock (ident, ["diagram","haskell"] , attrs) code) = do
-    let Just (_,outFile) = find (\(key, value) -> key == "outFile") attrs
-    writeFile "tmp.hs" code
-    system "ghc -O2 tmp.hs -o tmp -rtsopts > /dev/null"
-    system "./tmp +RTS -K100M > /dev/null"
-    return $ Div ("",[],[]) [(CodeBlock (ident, ["haskell"] , attrs) code)
-                            ,Para $ [Image nullAttr [Str "test"] (outFile,"test title")]
-                            ]
-insertDiagrams x = return x
-
--- | filter to insert dot file.
--- | Poor mans function to get a file extension
-getFileExtension fileName = reverse ext
-  where
-    (ext, _) = break (=='.') . reverse $ fileName
-
-insertDot :: Block -> IO Block
-insertDot (CodeBlock (ident, ["showDot"] , attrs) code) = do
-    writeFile "log" $ show attrs
-    let Just (_,outFile) = find (\(key, value) -> key == "outFile") attrs
-    let ext = getFileExtension outFile
-
-    writeFile "tmp.dot" code
-    system $ "dot tmp.dot -T" ++ ext ++ " > " ++ outFile
-    return $ Para $ [Image nullAttr [Str "test"] (outFile,"test title")]
-
-insertDot (CodeBlock (ident, ["showDotCode"] , attrs) code) = do
-    let Just (_,outFile) = find (\(key, value) -> key == "outFile") attrs
-    let ext = getFileExtension outFile
-
-    writeFile "tmp.dot" code
-    system $ "dot tmp.dot -T" ++ ext ++ " > " ++ outFile
-    return $ Div ("",[],[]) [(CodeBlock (ident, ["haskell"] , attrs) code)
-                            ,Para $ [Image nullAttr [Str "test"] (outFile,"test title")]
-                            ]
-insertDot x = return x
-
-
 -- Create a local copy of included inmages in pandoc. As long as local copy
 -- exists, do not use the file from given link.
 getFilename :: String -> String 
@@ -117,7 +40,7 @@ copyImagesLocally x = return x
 
 main :: IO ()
 main = do 
-    toJSONFilter handleCmds
-    toJSONFilter insertDiagrams 
-    toJSONFilter insertDot 
+    --toJSONFilter handleCmds
+    --toJSONFilter insertDiagrams 
+    --toJSONFilter insertDot 
     toJSONFilter copyImagesLocally
