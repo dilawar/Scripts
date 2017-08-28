1d67019801121061fe637b3f05a31802ec6fd961
Dilawar Singh <dilawars@ncbs.res.in>
2017-08-28 12:19:29 +0530

    pandoc2pdf.sh updated.

--- pandoc2pdf.sh
+++ pandoc2pdf.sh
@@ -2,18 +2,14 @@
 set -e
 set -x
 unset TEXMF
+PANDOC="pandoc -S -s -N -F pandoc-citeproc -F pandoc-crossref "
+
 # This script uses pandoc to convert markdown to pdf. 
 if [ $# -lt 1 ]; then
     echo "USAGE: ./$0 filename.pandoc [optional]"
     exit
 fi
 
-
-LATEX="lualatex -shell-escape"
-
 filename=$1
 outputFile="${filename%.pandoc}.pdf"
-
-pandoc -s -f markdown+latex_macros+tex_math_dollars --filter=graphviz.py $filename -o $outputFile
-exit;
-
+$(PANDOC) -tlatex $filename -o $outputFile
