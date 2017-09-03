f17a6b83c9325a35bf07e8a6935e2205d3cd2312
Dilawar Singh <dilawars@ncbs.res.in>
2017-09-03 13:00:27 +0530

    Added script, pandoc to beamer class.

---
+++ pandoc2beamer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PANDOC="pandoc -S -s -N \
+    --listings --template ${SCRIPT_DIR}/pandoc/templates/default.latex \
+    -F pandoc-crossref -F pandoc-citeproc \
+    -F ${SCRIPT_DIR}/pandoc/command.hs \
+    -F ${SCRIPT_DIR}/pandoc/include_code.hs "
+
+# This script uses pandoc to convert markdown to pdf. 
+if [ $# -lt 1 ]; then
+    echo "USAGE: ./$0 filename.pandoc [optional]"
+    exit
+fi
+
+filename=$1
+outputFile="${filename%.pandoc}.pdf"
+$PANDOC -tlatex $filename -o $outputFile
