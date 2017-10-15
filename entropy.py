36ecd088fa442690639fcb96ca21570e8dcc3d81
Dilawar Singh <dilawars@ncbs.res.in>
2017-10-15 11:37:35 +0530

    Compute entropy for give freqs or pros.

---
+++ entropy.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""entropy.py: 
+
+"""
+
+from __future__ import print_function, division
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import math
+
+def main( ):
+    args = [ eval(x.strip()) for x in sys.argv[1:] ]
+    if sum( args ) <= 1.0:
+        probs = args
+    else:
+        probs = [ x/ sum(args ) for x in args]
+
+    assert sum( probs ) == 1.0, "Must add up to 1"
+    ent = sum( [ -x * math.log(x, 2.0) for x in probs ] )
+    print( 'H=%f bits' % ent )
+    return ent
+
+
+if __name__ == '__main__':
+    main()
