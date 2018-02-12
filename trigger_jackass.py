b4deaa0c4433a07c3f6b0455236650828bf15509
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-12 11:17:59 +0530

    When triggering jackass, put data to positive folder.

--- trigger_jackass.py
+++ trigger_jackass.py
@@ -18,16 +18,34 @@ import serial
 import time
 import sounddevice as sd 
 import datetime
+import glob
+import shutil
+
+datadir = os.path.join( os.environ['HOME'], 'Work', 'DATA', 'JACKASS' )
+
+positiveData = os.path.join( os.environ['HOME'], 'Work', 'DATA', 'JACKASS', 'POSITIVE' )
+if not os.path.exists( positiveData ):
+    os.makedirs( positiveData )
 
 def main( ):
     ser = serial.Serial( '/dev/ttyACM0', 38400, timeout = 0.5 )
     try:
         ser.write( 'P'.encode( ) )
+        list_of_files = glob.glob('%s/*.png' % datadir) 
+        latest_file = max(list_of_files, key=os.path.getctime)
+        # More the last spectogram file to positive 
+        shutil.copyfile( latest_file, positiveData )
     except KeyboardInterrupt as e:
         print( "keyboad interrupt" )
         ser.close()
+    except Exception as e:
+        ser.close( )
+        print( e )
 
-    ser.close()
+    try:
+        ser.close()
+    except Exception as e:
+        pass
 
 if __name__ == '__main__':
     main()
