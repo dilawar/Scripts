3330c18b3d049e24530e7f77883457935d4c369b
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-17 14:59:54 +0530

    mic live.

---
+++ mic_live.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+## This is an example of a simple sound capture script.
+##
+## The script opens an ALSA pcm for sound capture. Set
+## various attributes of the capture, and reads in a loop,
+## Then prints the volume.
+##
+## To test it out, run it and shout at your microphone:
+
+import alsaaudio, time, audioop
+import datetime
+
+# Open the device in nonblocking capture mode. The last argument could
+# just as well have been zero for blocking mode. Then we could have
+# left out the sleep call in the bottom of the loop
+inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
+
+# Set attributes: Mono, 8000 Hz, 16 bit little endian samples
+inp.setchannels(1)
+inp.setrate(8000)
+inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+
+# The period size controls the internal number of frames per period.
+# The significance of this parameter is documented in the ALSA api.
+# For our purposes, it is suficcient to know that reads from the device
+# will return this many frames. Each frame being 2 bytes long.
+# This means that the reads below will return either 320 bytes of data
+# or 0 bytes of data. The latter is possible because we are in nonblocking
+# mode.
+inp.setperiodsize(160)
+
+samples = [ ]
+
+while True:
+    # Read data from device
+    l,data = inp.read()
+    if l:
+        # Return the maximum of the absolute value of all samples in a fragment.
+        samples.append( audioop.rms(data, 2) )
+
+    if len( samples ) == 100:
+        stamp = datetime.datetime.now().isoformat( )
+        print( '%s, %d' % (stamp, sum( samples ) / len( samples )) )
+        samples = [ ]
