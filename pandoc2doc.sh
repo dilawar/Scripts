4732816abe603cce79ba498876898de8672ef67a
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-08 10:53:32 +0530

    Shift args after consumption.

--- pandoc2doc.sh
+++ pandoc2doc.sh
@@ -10,12 +10,10 @@
 set -e
 set -o nounset                                  # Treat unset variables as an error
 
-PANDOC="$1";shift
-EXT=${1:-docx}
+PANDOC="$1";shift;
+EXT=${1:-docx};shift;
 
 echo "generating docx"
 cat $PANDOC | $HOME/Scripts/pandoc/preprocess_of_docx.py | \
     pandoc -F $HOME/Scripts/pandoc/code_blocks.py \
-    -F pandoc-crossref -F pandoc-citeproc \
-    "$@" \
-    -o $PANDOC.$EXT
+    -F pandoc-crossref -F pandoc-citeproc "$@" -o $PANDOC.$EXT
