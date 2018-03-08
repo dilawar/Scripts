e1ffc2207301c7a7a056ab2dbad5a7f38e8ad4e4
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-08 21:20:11 +0530

    Count in pdf.

---
+++ pdf_count.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: pdf_count.sh
+#
+#         USAGE: ./pdf_count.sh
+#
+#   DESCRIPTION:  Count words in pdf. User can pass first argument to this
+#   script to count something else.
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: Thursday 08 March 2018 09:17:21  IST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+PDFFILE=$1;shift
+WHAT=${1:--w}
+pdftotext $PDFFILE -enc UTF-8 - | wc $WHAT
