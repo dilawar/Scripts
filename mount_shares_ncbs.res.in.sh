b56a5825c8a5af3dbbf55df3eed8b89580799982
dilawars <dilawars@pantua.ncbs.res.in>
2018-03-26 10:59:01 +0530

    mount shares.

--- mount_shares_ncbs.res.in.sh
+++ mount_shares_ncbs.res.in.sh
@@ -5,4 +5,6 @@ if [ ! -d $MOUNT_POINT ]; then
     mkdir -p $MOUNT_POINT
 fi
 echo "Mouting shares.ncbs.res.in"
-sshfs dilawars@shares.ncbs.res.in:/home/bhalla/dilawars $MOUNT_POINT
+#sshfs dilawars@shares.ncbs.res.in:/home/bhalla/dilawars $MOUNT_POINT
+mount -t cifs //shares.ncbs.res.in/dilawars -o vers=3.0,rw,uid=50066,gid=50009,username=dilawars, $HOME/shares.ncbs.res.in
+
