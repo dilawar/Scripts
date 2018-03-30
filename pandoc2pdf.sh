3c541b106b1d665907b5dd5f04a3e2b6b78e8012
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-30 18:24:26 +0530

    Add filter only if they exists.

--- pandoc2pdf.sh
+++ pandoc2pdf.sh
@@ -2,10 +2,33 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PANDOC="pandoc -s -N \
-    -F pandoc-imagine \
-    -F $SCRIPT_DIR/pandoc/dilawar.py \
-    -F pandoc-crossref -F pandoc-citeproc "
+PANDOC_FILTERS= -F $SCRIPT_DIR/pandoc/dilawar.py 
+
+if [ -x "$(pandoc-citeproc -v)" ]; then
+    PANDOC_FILTERS="$PANDOC_FILTERS -F pandoc-citeproc"
+else
+    echo "[WARN] pandoc-citeproc not found"
+fi
+if [ -x "$(pandoc-crossref -v)" ]; then
+    PANDOC_FILTERS="$PANDOC_FILTERS -F pandoc-crossref"
+else
+    echo "[WARN] pandoc-crossref not found"
+fi
+
+if which pandoc-imagine > /dev/null; then
+    PANDOC_FILTERS="$PANDOC_FILTERS -F pandoc-imagine"
+else
+    echo "[WARN] pandoc-imagine not found"
+fi
+
+if which pantable > /dev/null; then
+    PANDOC_FILTERS="$PANDOC_FILTERS -F pantable"
+else
+    echo "[WARN] pantable not found"
+fi
+
+
+PANDOC="pandoc -s -N $PANDOC_FILTERS"
 
 # This script uses pandoc to convert markdown to pdf. 
 if [ $# -lt 1 ]; then
