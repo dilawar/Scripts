8d5ae95317e0b5bf76760f991a7f296ca43753f2
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-30 20:25:28 +0530

    Working fine.

--- pandoc2pdf.sh
+++ pandoc2pdf.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PANDOC_FILTERS=$($SCRIPT_DIR/pandoc_find_filters.sh)
@@ -15,4 +16,5 @@ filename=$1; shift
 outputFile="${filename%.pandoc}.pdf"
 outTex="${filename%.pandoc}.tex" 
 $PANDOC $filename "$@" -o $outTex 
-latexmk -pdf -lualatex -shell-escape -silent $outTex || lualatex --shell-escape $outTex
+# latexmk -pdf -lualatex -shell-escape -silent $outTex || lualatex --shell-escape $outTex
+lualatex --shell-escape $outTex
