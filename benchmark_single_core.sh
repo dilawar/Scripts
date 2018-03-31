46716e17f3930314af97291b72df324581c6a1e4
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-31 10:28:31 +0530

    Run stress-ng test.

--- benchmark_single_core.sh
+++ benchmark_single_core.sh
@@ -24,3 +24,17 @@ set -x -e
 echo 'int main() { double i=0;for(i=0;i<5000000000;i++) {20%7 * i; }}' > \
     /tmp/test.c && gcc -O3 /tmp/test.c -o /tmp/test \
     && time /tmp/test && rm /tmp/test.c && rm /tmp/test
+
+echo "[INFO] Running stress-ng"
+stress-ng --cpu 4 --cpu-method matrixprod  --metrics-brief --perf -t 60
+
+echo "[INFO] Execute phoronix test suite by yourself"
+#if [ ! -d phoronix-test-suite ]; then
+#    git clone --depth 10 https://github.com/phoronix-test-suite/phoronix-test-suite
+#fi
+#(
+#    cd ./phoronix-test-suite
+#    sudo bash -c ./install-sh
+#)
+
+# phoronix-test-suite batch-setup
