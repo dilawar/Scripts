888c58042440a1a41fddfae9c21cc277510b8a63
Dilawar Singh <dilawars@ncbs.res.in>
2018-06-23 14:26:40 +0530

    Added docstring.

--- html_self_contained.sh
+++ html_self_contained.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# This script turn a download html page into self-contained html i.e. all images
+# are turned into base64 strings. You must have pandoc installed.
 pandoc -f html -t html --selfcontained "$1"
