a2ae755e718c38b94bec1c9a8b6a4e759d42da6c
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-06 16:53:42 +0530

    md2html filter.

--- md2html.sh
+++ md2html.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
-
 set -e
-set -o nounset                                  # Treat unset variables as an error
-
-PANDOC_FILE="$1"
-EXT=${2:-html}
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PANDOC_FILTERS="$($SCRIPT_DIR/./pandoc_find_filters.sh)"
-echo "generating docx"
-cat $PANDOC_FILE | $HOME/Scripts/pandoc/preprocess_of_html.py | \
-    pandoc $PANDOC_FILTERS \
-    --standalone --self-contained --mathml \
-    -o $PANDOC_FILE.$EXT
+PANDOC_FILTERS=$($SCRIPT_DIR/pandoc_find_filters.sh)
+PANDOC="pandoc -s -N $PANDOC_FILTERS --lua-filter $SCRIPT_DIR/tikz2pdf.lua"
+
+filename=$1; shift
+outputFile="${filename}.html"
+$PANDOC $filename "$@" -t html --self-contained -o $outTex 
