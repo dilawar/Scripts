7f62fde68a962a028fe712c2c47ad449cabc037f
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-26 13:29:45 +0530

    Create a zip out of given directory.

---
+++ zip_recursive.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+set -x
+# ZIP the given directory.
+DIRPATH=`realpath ${1-.}`
+DIRNAME=$(basename $(realpath $DIRPATH))
+ZIPNAME=${DIRPATH}.zip
+zip -9 -r $ZIPNAME $DIRPATH -x *.git* -x *.svn* -x *.DS_Store*
+echo "Created $ZIPNAME"
+ls -lh $ZIPNAME
