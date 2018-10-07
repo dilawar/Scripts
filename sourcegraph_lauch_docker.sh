5482381b89f1b98fc499a77227653e8848427ba4
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-07 11:44:06 +0530

    Cleanup bash for centos6.

--- sourcegraph_lauch_docker.sh
+++ sourcegraph_lauch_docker.sh
@@ -3,4 +3,4 @@ docker run --publish 7080:7080 \
     --rm --volume ~/.sourcegraph/config:/etc/sourcegraph \
     --volume ~/.sourcegraph/data:/var/opt/sourcegraph \
     --volume /var/run/docker.sock:/var/run/docker.sock \
-    sourcegraph/server:2.11.2
+    sourcegraph/server:latest
