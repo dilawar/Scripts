97b642ba9015a65419defddb2330466d34e4542d
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-18 18:42:20 +0530

    Shell script to download and run textidote.sh

--- textidote.sh
+++ textidote.sh
@@ -4,6 +4,6 @@ JARFILE=$HOME/Scripts/textidote.jar
 
 if [ ! -f $JARFILE ]; then
     echo "Downloading required jar file."
-    wget -o $JARFILE https://github.com/sylvainhalle/textidote/releases/download/v0.6/textidote.jar 
+    wget -O $JARFILE https://github.com/sylvainhalle/textidote/releases/download/v0.6/textidote.jar 
 fi
 java -jar $JARFILE "$@"
