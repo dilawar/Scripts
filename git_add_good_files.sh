e9d66de44612f1ed332360d4bd8e44b233dc0fcd
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-27 18:40:10 +0530

    Add file.

--- git_add_good_files.sh
+++ git_add_good_files.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env bash
-find . -type f -regex ".+\.(txt|md|pandoc|tex|py|cpp|c|h)"
+
+is_inside_git_repo() {
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1
+}
+while IFS= read -r -d '' file; do
+    _dir=$(dirname $file)
+    (
+        cd $_dir
+        filename=$(basename $file)
+        is_inside_git_repo || continue
+        git add $filename || echo "File $filename is not added"
+    )
+done < <(find . -type f \
+    -regextype posix-extended \
+    \( -regex ".*\.(md|pandoc|tex|py|cpp|c|cxx|h|hpp|html|css)$" \
+    \) -print0)
