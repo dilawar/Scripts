b64029f2075ec6150a55c480912e13b07a26b21a
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-29 10:43:34 +0530

    Updated remind.sh file.

--- remind.sh
+++ remind.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
-# Add this to crontab 
-# 0,15,30,45 * * * * ~/Scripts/remind.sh
 
-# This is from here  https://unix.stackexchange.com/a/111190/5362
-DBUSFILE=$HOME/.dbus/Xdbus
-touch $DBUSFILE
-chmod 600 $DBUSFILE
-env | grep DBUS_SESSION_BUS_ADDRESS > $DBUSFILE
-echo 'export DBUS_SESSION_BUS_ADDRESS' >> $DBUSFILE
-# Now source this file.
-. $DBUSFILE
-export DISPLAY=:0
-export XAUTHORITY=$HOME/.Xauthority
-gcalcli remind 15
+LOCKFILE=/tmp/_remind.lock
+if [ `flock -xn $LOCKFILE -c 'echo 1'` ]; then
+    echo "Starting remind service"
+else
+   echo -n 'its already running with PID'
+   cat $LOCKFILE
+   exit 1 
+fi
+
+# if gcalcli is not found. do nothing.
+
+
+if ! [ -x "$(command -v gcalcli)" ]; then
+    echo "gcalcli is not installed."
+    exit 1;
+fi
+
+while true; do
+    gcalcli --calendar="dilawar" remind
+    sleep 300
+done &
