f4385875f34a24a3c5d8de9b4dbe6ea7b59af67a
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-29 10:59:24 +0530

    Added shell script to run gcalcli process.

--- remind.sh
+++ remind.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
+set -e
+(
+    flock -x -w 1 200 || (echo "Already running"; exit 1)
 
-LOCKFILE=/tmp/_remind.lock
-if [ `flock -xn $LOCKFILE -c 'echo 1'` ]; then
-    echo "Starting remind service"
-else
-   echo -n 'its already running with PID'
-   cat $LOCKFILE
-   exit 1 
-fi
-
-# if gcalcli is not found. do nothing.
+    # if gcalcli is not found. do nothing.
 
 
-if ! [ -x "$(command -v gcalcli)" ]; then
-    echo "gcalcli is not installed."
-    exit 1;
-fi
+    if ! [ -x "$(command -v gcalcli)" ]; then
+        echo "gcalcli is not installed."
+        exit 1;
+    fi
 
-while true; do
-    gcalcli --calendar="dilawar" remind
-    sleep 300
-done &
+    while true; do
+        gcalcli --calendar="dilawar" remind
+        sleep 300
+    done &
+) 200> $HOME/.cache/remind.sh.exclusivelock
