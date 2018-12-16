fd56d7c2a58eea4d01e93f96425fc6cf59fe29fa
Dilawar Singh <dilawars@ncbs.res.in>
2018-12-16 09:58:39 +0530

    updated spacevim.d directory.

--- spacevim_install.sh
+++ spacevim_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 curl -sLf https://spacevim.org/install.sh | bash
-if [ ! -d $HOME/.Spacevim.d ]; then
-    git clone https://github.com/dilawar/.SpaceVim.d ~/.SpaceVim.d 
+if [ ! -d $HOME/.SpaceVim.d ]; then
+  git clone https://github.com/dilawar/.SpaceVim.d ~/.SpaceVim.d 
+else
+  cd $HOME/.SpaceVim.d/ && git pull 
 fi
