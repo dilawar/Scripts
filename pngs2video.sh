8331b6e2bde50332b508ed28ac6aa6bd98ce0649
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-03-11 19:33:39 +0530

    Trying to reduce the frame rate.

--- pngs2video.sh
+++ pngs2video.sh
@@ -6,6 +6,7 @@ echo "First argument to this script is filename pattern"
 PAT="$1"
 ffmpeg -pattern_type glob -i "$PAT" -vcodec libtheora \
     -b 9000k \
-    -r 24 \
+    -framerate 10\
+    -r 10\
     -pix_fmt yuv444p \
     output.ogv
