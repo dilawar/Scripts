57615edf7e63d143ff61dddcfd489927fb3cb689
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-04-18 11:34:41 +0530

    Script to build and check package.

--- entropy.py
+++ entropy.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """entropy.py: 
 
 """
-
-from __future__ import print_function, division
     
 __author__           = "Dilawar Singh"
 __copyright__        = "Copyright 2017-, Dilawar Singh"
@@ -14,7 +12,6 @@ __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
 import sys
-import os
 import math
 
 def main( ):
@@ -24,7 +21,7 @@ def main( ):
     else:
         probs = [ x/ sum(args ) for x in args]
 
-    assert abs(sum( probs ) - 1.0) < 1e-6, "Must add up to 1: %s" %  probs
+    assert abs(sum(probs)-1.0) < 1e-6, f"Must add up to 1: {sum(probs)}"
     ent = sum( [ -x * math.log(x, 2.0) for x in probs ] )
     print( 'H=%f bits' % ent )
     return ent
