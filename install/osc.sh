23e6e6594b3806daf30caf14547456d663ed20a2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-01-31 18:03:56 +0530

    build scripts are working...

--- install/osc.sh
+++ install/osc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-[ -d /tmp/osc ] || git clone --depth 10 https://github.com/openSUSE/osc.git /tmp/osc
+[ -d /tmp/osc ] || git clone --depth 10 https://github.com/openSUSE/osc /tmp/osc
 (
-    cd /tmp/osc.git
+    cd /tmp/osc
     python setup.py build
     python setup.py install --user
 )
