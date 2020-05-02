5ffa7e83d52f4b8bcae1541df211d22a8bcbeee4
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-02 21:39:47 +0530

    add local scripts.

--- docker_with_my_profile.sh
+++ docker_with_my_profile.sh
@@ -6,4 +6,5 @@ docker run -d \
     -it \
     --mount type=bind,source="$(HOME)"/.vim,target=/root/.vim,readonly \
     --mount type=bind,source="$(HOME)"/.gitconfig,target=/root/.gitconfig,readonly \
+    --mount type=bind,source="$(HOME)"/Scripts,target=/root/Scripts \
     bash
