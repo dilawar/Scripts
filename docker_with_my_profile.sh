62c3bdc41d055a75488ded149f6e8e31ecae1775
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-02 23:38:34 +0530

    vim is not readonly. Undoddir needs to be writable.

--- docker_with_my_profile.sh
+++ docker_with_my_profile.sh
@@ -2,7 +2,7 @@
 set -u -x -e
 NAME="$1"
 docker run \
-    --mount type=bind,source="${HOME}"/.vim,target=/root/.vim,readonly \
+    --mount type=bind,source="${HOME}"/.vim,target=/root/.vim \
     --mount type=bind,source="${HOME}"/.gitconfig,target=/root/.gitconfig,readonly \
     --mount type=bind,source="${HOME}"/Scripts,target=/root/Scripts \
     -it ${NAME} bash
