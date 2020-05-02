4f75935e60baaae6d830a6d625df107fa986e3b6
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-02 21:47:58 +0530

    run docker with my profile.

--- docker_with_my_profile.sh
+++ docker_with_my_profile.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -o
-set -e
+set -u -x -e
 NAME="$1"
-docker run -d \
-    -it \
-    --mount type=bind,source="${HOME}"/.vim,target=/root/,readonly \
-    --mount type=bind,source="${HOME}"/.gitconfig,target=/root,readonly \
-    --mount type=bind,source="${HOME}"/Scripts,target=/root\
-    ${NAME} bash
+docker run \
+    --mount type=bind,source="${HOME}"/.vim,target=/root/.vim,readonly \
+    --mount type=bind,source="${HOME}"/.gitconfig,target=/root/.gitconfig,readonly \
+    --mount type=bind,source="${HOME}"/Scripts,target=/root/Scripts \
+    -it ${NAME} bash
