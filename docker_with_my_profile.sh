ae0012789ac9ee6bb46766b57ed2f384715f1320
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-02 21:39:02 +0530

    Docker run with my profile.

---
+++ docker_with_my_profile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o
+set -e
+NAME="$1"
+docker run -d \
+    -it \
+    --mount type=bind,source="$(HOME)"/.vim,target=/root/.vim,readonly \
+    --mount type=bind,source="$(HOME)"/.gitconfig,target=/root/.gitconfig,readonly \
+    bash
