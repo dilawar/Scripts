fb52a46def190cc836f37a30f3addc263dd635b2
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-02 21:42:24 +0530

    () -> {}

--- docker_with_my_profile.sh
+++ docker_with_my_profile.sh
@@ -4,7 +4,7 @@ set -e
 NAME="$1"
 docker run -d \
     -it \
-    --mount type=bind,source="$(HOME)"/.vim,target=/root/.vim,readonly \
-    --mount type=bind,source="$(HOME)"/.gitconfig,target=/root/.gitconfig,readonly \
-    --mount type=bind,source="$(HOME)"/Scripts,target=/root/Scripts \
-    bash
+    --mount type=bind,source="${HOME}"/.vim,target=/root/.vim,readonly \
+    --mount type=bind,source="${HOME}"/.gitconfig,target=/root/.gitconfig,readonly \
+    --mount type=bind,source="${HOME}"/Scripts,target=/root/Scripts \
+    ${NAME} bash
