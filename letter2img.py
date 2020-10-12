7ba7add6c93a5c790219a84cbbef4869bb064be0
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-10-12 18:38:10 +0530

    updated a bit more.

--- letter2img.py
+++ letter2img.py
@@ -7,6 +7,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 args_ = None
 
+
 def main():
     global args_
     W = int(args_.size)
@@ -14,10 +15,10 @@ def main():
     img = Image.new("L", (W, W), 10)
     draw = ImageDraw.Draw(img)
     FONT = ImageFont.truetype(args_.font, font_scale)
-    FONT_COLOR=255
+    FONT_COLOR = 255
     T = args_.LETTER
-    draw.text((W//5, 0), f"{T}", font=FONT, fill=FONT_COLOR) 
-    img.save(f'{T}.png')
+    draw.text((W // 5, 0), f"{T}", font=FONT, fill=FONT_COLOR)
+    img.save(f"{T}.png")
 
 
 if __name__ == "__main__":
@@ -25,9 +26,21 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("LETTER", help="Letter to convert")
-    parser.add_argument("--size", "-s", default=10, type=int, help="Image will be wxw")
-    parser.add_argument("--pen-width", "-p", default=0, type=int, help="Pen width in pixel. (default auto)")
-    parser.add_argument("--font", "-F", default='DejaVuSansMono.ttf',
-            help="Which font to use (ttf)")
+    parser.add_argument(
+        "--size", "-s", default=10, type=int, help="Image size will be w×w"
+    )
+    parser.add_argument(
+        "--pen-width",
+        "-p",
+        default=0,
+        type=int,
+        help="Pen width in pixel (default: compute automatically)",
+    )
+    parser.add_argument(
+        "--font",
+        "-F",
+        default="DejaVuSansMono.ttf",
+        help="Which font to use. Default DejaVuSansMono.ttf",
+    )
     args_ = parser.parse_args()
     main()
