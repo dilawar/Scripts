ea623b981b17c638f278a09a759348c305cade8b
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-10-12 18:11:04 +0530

    Letter to image script.

---
+++ letter2img.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+__author__ = "Dilawar Singh"
+__email__ = "dilawar@subcom.tech"
+
+from PIL import Image, ImageDraw, ImageFont
+
+args_ = None
+
+def main():
+    global args_
+    W = int(args_.size)
+    font_scale = W
+    img = Image.new("L", (W, W), 10)
+    draw = ImageDraw.Draw(img)
+    FONT = ImageFont.truetype(args_.font, font_scale)
+    FONT_COLOR=255
+    T = args_.LETTER
+    draw.text((W//5, 0), f"{T}", font=FONT, fill=FONT_COLOR) 
+    img.save(f'{T}.png')
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("LETTER", help="Letter to convert")
+    parser.add_argument("--size", "-s", default=10, type=int, help="Image will be wxw")
+    parser.add_argument("--pen-width", "-p", default=0, type=int, help="Pen width in pixel. (default auto)")
+    parser.add_argument("--font", "-F", default='DejaVuSansMono.ttf',
+            help="Which font to use (ttf)")
+    args_ = parser.parse_args()
+    main()
