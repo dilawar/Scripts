98db12893c3f31968a13ed5492b38f1045a002ff
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-11-08 17:54:51 +0530

    Added script to install neovim

--- install_neovim_appimage.sh
+++ install_neovim_appimage.sh
@@ -12,6 +12,18 @@
 	# $HOME/.local/bin/nvim.appimage
 	chmod a+x nvim.appimage
 	./nvim.appimage --appimage-extract 
+
+	if [ ! -d ~/.config/nvim ]; then
+		cd ~/.config && git clone https://github.com/dilawar/nvim --recursive
+	fi
 	echo "Make sure ~/squashfs-root/usr/bin is in path"
+
+	# From https://stackoverflow.com/a/28021305/1805129
+	LINE='alias vim=$HOME/squashfs-root/usr/bin/nvim'
+	FILE=$HOME/.bashrc
+	grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+	source ~/.bashrc
 )
+
 
