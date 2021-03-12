55c14b14ee92cb70217b0804f72e8379515bf97e
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-03-12 14:18:59 +0530

    copy script is updated.

--- copy_to_clipboard.sh
+++ copy_to_clipboard.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ ! -f $1 ]; then
+if [ ! -f "$1" ]; then
     echo "First argument must be a valid file path."
     echo " Given : $1"
     exit;
 fi
-xsel -i -b < $1
+xsel -i -b < "$1"
 echo "Copied $1 to clipboard"
