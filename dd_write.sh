a0da4e83bcf931969c28cbf2f5e0b499f19a3ef2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-05-01 14:15:12 +0530

    updated dd_write

--- dd_write.sh
+++ dd_write.sh
@@ -2,4 +2,6 @@
 set -u
 set -x
 set -e
-sudo dd if=$1 of=$2 conv=noerror,sync bs=4M status=progress
+sudo dd if=$1 of=$2 conv=sync bs=4M status=progress
+sudo sync
+notify-send "Done writing $1 to $2"
