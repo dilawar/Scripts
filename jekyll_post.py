b9bf99a2207cc8344c6188d9f9ff1e186e150a07
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-05-30 18:53:27 +0530

    Auto :

--- jekyll_post.py
+++ jekyll_post.py
@@ -6,23 +6,23 @@ import typing as T
 from pathlib import Path
 
 
-def main(title: str, postdir : T.Optional[Path] = None):
-    today = datetime.datetime.now().strftime('%Y-%m-%d')
-    year = datetime.datetime.now().strftime('%Y')
+def main(title: str, postdir: T.Optional[Path] = None):
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    year = datetime.datetime.now().strftime("%Y")
     if postdir is None:
-        postdir = Path('.').parent / '_posts' / year
+        postdir = Path(".").parent / "_posts" / year
     else:
         postdir = Path(postdir)
     postdir.mkdir(parents=True, exist_ok=True)
-    fname = postdir / f"{title.replace(' ', '-')}.md"
+    fname = postdir / f"{today}-{title.replace(' ', '-')}.md"
     assert not fname.exists(), f"{fname} already exists"
     with fname.open("w") as f:
-        f.write('---\n')
-        f.write(f'title: {title}\n')
-        f.write(f'date: {today}\n')
-        f.write(f'comments: true\n')
-        f.write('---\n')
-    print(f'Saved to {fname}')
+        f.write("---\n")
+        f.write(f"title: {title}\n")
+        f.write(f"date: {today}\n")
+        f.write(f"comments: true\n")
+        f.write("---\n")
+    print(f"Saved to {fname}")
 
 
 if __name__ == "__main__":
