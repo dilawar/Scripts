fd873d4082bd477d78a167a6613351e76fd3687a
Dilawar Singh <dilawar@subcom.tech>
2021-05-30 12:00:54 +0530

    Auto :

---
+++ install/pylibs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+python3 -m pip install pip --upgrade --user
+python3 -m pip install \
+    lxml \
+    poetry \
+    numpy \
+    --upgrade
--- scripts_update.sh
+++ scripts_update.sh
@@ -2,7 +2,7 @@
 ( 
     cd $HOME/Scripts
     git add *.sh *.py
-    find . -type f -executable -print0 | xargs -0 -I file git add file
+    find . -type f -not -path '*/\.*' -executable -print0 | xargs -0 -I file git add file
     git add -u :/
     git commit -m "Auto : $1" 
     git push 
