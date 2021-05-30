3fb6e06189e2243241654f69812399f7e3980897
Dilawar Singh <dilawar@subcom.tech>
2021-05-30 11:43:46 +0530

    Auto :

---
+++ install_nix.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+curl -L https://nixos.org/nix/install | sh
--- scripts_update.sh
+++ scripts_update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd $HOME/Scripts && git add -u && \
-    git commit -m "Auto : $1" && git push && cd
-
+( 
+    cd $HOME/Scripts
+    git add *.sh *.py
+    find . -type f -executable -print0 | xargs -0 -I file git add file
+    git add -u :/
+    git commit -m "Auto : $1" 
+    git push 
+)
