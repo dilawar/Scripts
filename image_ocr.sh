8ec95a9834348e402395298246f07b4c2887933e
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-05-31 22:50:30 +0530

    image_ocr.sh using tesseract.

--- image_ocr.sh
+++ image_ocr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-FILENAME="$1"
-tesseract "$FILENAME" /tmp/output -l eng+hin
-cat /tmp/output.txt
+FILENAME="$1"; shift;
+tesseract "$FILENAME" /tmp/output -l eng $@
+sed -e /^\ *$/d /tmp/output.txt
