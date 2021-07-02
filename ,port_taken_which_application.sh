479b7d9f029386b27ef52cc518fe36c98be564c0
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-07-02 11:08:39 +0530

    remove.

--- ,port_taken_which_application.sh
+++ ,port_taken_which_application.sh
@@ -14,20 +14,16 @@ PORT="$1"
 
 echo "Looking for application using port $PORT"
 
-if command -v lsof &> /dev/null
+if command -v ss &> /dev/null
 then
-    sudo lsof -i :$PORT
+    sudo ss -tulpn | grep :$PORT
+
+    exit;
 fi
 
 if command -v netstat &> /dev/null
 then
-    netstat -tulpn | grep ":$PORT"
+    netstat -tulpn | grep :$PORT
     exit;
 fi
 
-if command -v ss &> /dev/null
-then 
-    sudo ss -tulp | grep ":$PORT"
-    
-    exit;
-fi
