849a51fa43bdf862093766bc24d73281b7811c57
Dilawar Singh <dilawar.s.rajput@gmail.com>
2021-07-08 10:26:39 +0000

    Added aws_bootstrap image for centos

---
+++ aws_bootstrap_centos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x 
+set -e
+
+sudo dnf install -y epel-release dnf-plugins-core
+sudo dnf config-manager --set-enabled powertools
+sudo dnf upgrade
+sudo dnf install -y curl wget git cmake nmap htop neovim mutt
+
+# docker
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y docker-ce --nobest
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo dnf install -y python39 python39-devel 
+./bootstrap.sh
