e263d76efe994e9c18f865a8ba1c293843a27f22
Dilawar Singh <dilawar@subcom.tech>
2023-05-28 09:15:18 +0530

    minor tweaks

--- ,git_list_commits_for_a_path.sh
+++ ,git_list_commits_for_a_path.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-set -n 
 set -x 
 set -e
+set -n
 
 FILENAME=$1
+echo "Listing commits for $FILENAME"
 git log --all --first-parent --remotes --reflog --author-date-order -- $FILENAME
