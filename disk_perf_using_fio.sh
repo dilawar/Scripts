8d6e5a8d485950530cb1c9b93adbd6e49af6a1b3
Dilawar Singh <dilawar@subcom.tech>
2024-08-11 22:33:08 +0530

    chore: Adds docs

--- disk_perf_using_fio.sh
+++ disk_perf_using_fio.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Thanks https://webhostinggeeks.com/howto/use-fio-measure-speed-data-reads-writes-linux/
+
 fio --name=test --ioengine=sync --rw=randwrite --bs=4k --numjobs=1 --size=1G --runtime=30s --time_based
