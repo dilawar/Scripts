30c6589b296df66be700ef4056271764cb1cd43f
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-05-10 14:59:57 +0530

    for latest zypper versions

--- zypper_autoremove.sh
+++ zypper_autoremove.sh
@@ -4,4 +4,6 @@ set -xe
 
 # Thanks https://github.com/openSUSE/zypper/issues/116#issuecomment-774486027
 # and https://github.com/openSUSE/zypper/issues/116#issuecomment-1492793358
-zypper packages --unneeded | awk -F'|' 'NR==0 || NR==1 || NR==2 || NR==3 || NR==4 {next} {print $3}' | grep -v Name | sudo xargs zypper remove --clean-deps
+# zypper packages --unneeded | awk -F'|' 'NR==0 || NR==1 || NR==2 || NR==3 || NR==4 {next} {print $3}' | grep -v Name | sudo xargs zypper remove --clean-deps
+
+zypper pa --unneeded | grep '^i' | cut -d\| -f3 | xargs sudo zypper -n remove
