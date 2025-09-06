e224b68c4488cb644826cd20f41659bd8ac37545
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-09-06 23:29:13 +0530

    default config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,33 @@
-set PATH $HOME/.mutt $HOME/Scripts $HOME/Scripts/mypandoc $PATH 
-set http_proxy http://proxy.ncbs.res.in:3128
-set https_proxy http://proxy.ncbs.res.in:3128
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Thanks https://fishshell.com/docs/current/interactive.html
+function fish_hybrid_key_bindings --description \
+"Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings fish_hybrid_key_bindings
+
+set ANDROID_HOME ~/Android/Sdk
+set ANDROID_NDK_HOME ~/Android/Sdk/ndk/29.0.13113456/
+
+set ONEAPI_TOKEN woof-flattop-rascal-flakily
+set COMPOSER_MEMORY_LIMIT -1
+
+set PATH /usr/local/bin $PATH
+set PATH ~/.mutt/ $PATH
+set PATH /home/dilawar/Scripts $PATH
+set COMPOSER_MEMORY_LIMIT -1
+
+# Created by `pipx` on 2025-05-30 07:32:31
+set PATH $PATH /home/dilawar/.local/bin
+
+alias python=python3
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
